be706d5c5c93dc890ff4986376113002383ff9c9
metracambodia <metracambodia@gmail.com>
2026-06-09 17:19:46 +0700

    change

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,4 @@
-import DecodedResistorValue from "./resistor-color-trio";
+import {decodedResistorValue} from "./resistor-color-trio";
 
-//DecodedResistorValue(["white", "white", "white"]);
-//decodedResistorValue(["brown","black"]);
-//DecodedResistorValue(['brown','red','black']);
-
-const charactor: string[] = ['a','b','c','d'];
-
-//ternery 
-charactor.length > 5 && console.log(">2");
+console.log(decodedResistorValue(["black", "black", "black"]));//331
+//console.log(decodedResistorValue(["white", "whitei", "white"]));
--- src/resistor-color-trio.ts
+++ src/resistor-color-trio.ts
@@ -1,76 +1,108 @@
 
-import type {ColorTypes} from "./types";
-const COLORS:string[] = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
-export default function DecodedResistorValue(colorNameList: ColorTypes[]) {
+import type {ColorTypes} from "./types"; const COLORS:string[] = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
+export function decodedResistorValue(colorNameList: ColorTypes[]): string {
     let tempListNumber: number[] = [];
     let lastNumber:number=0;
     let appendString:string = '0';
     let numbericColor: string = "";
     let totalNumeric:number = 0;
-    // 1. ohms 100
-    //"black" | "brown" | "red" | "orange" | "yellow" | "green" | "blue" | "violet" | "grey" | "white";
-
-    //Ex: ['brown', 'green', 'red']
-    //result: 0100
-    //check list of collor array first
-    if(colorNameList.length > 2){
-        colorNameList.map((color) =>{
-            tempListNumber.push( COLORS.indexOf(color) );
-        });
+    
+    tempListNumber = findOutIndexNumeric(colorNameList);
         //find name of numberic color
       //let num = colorNameList.map((colorTitle,index)=> index < 2 && COLORS.indexOf(colorTitle));
-      for (let i: number = 0; i < 2; i++){
-        let temp:string = "";
-        temp = (COLORS.indexOf(colorNameList[i])).toString();
-        numbericColor = numbericColor + temp; 
-      }
-    }else{
-        console.log("array < 2")
-    }
-    // find the last number
-    if(lastNumber == 0){
-      lastNumber = tempListNumber[tempListNumber.length -1];
-    } 
-    if(lastNumber===2){
-      // 012
-      appendString='00';
-
-    }else if(lastNumber===3){
-      appendString='000';
-    }else if(lastNumber===4){
-      appendString='0000';
-    }else if(lastNumber===5){
-      appendString='00000';
-    }else if(lastNumber===6){
-      appendString='000000';
-    }else if(lastNumber===7){
-      appendString='0000000';
-    }else if(lastNumber===8){
-      appendString='00000000';
-    }else{
-      appendString='00000000000';
-    }
+    numbericColor = tempListNumber.length > 2 ? findTwoDigit([tempListNumber[0], tempListNumber[1]]) : "";
     
-    console.log(`result ${numbericColor}${appendString}`);
-    appendString = numbericColor + appendString;
-    totalNumeric = Number(appendString);
-    if( lastNumber === 3 ){
-      console.log(`${numbericColor} kiloohms`);
-    }else if(lastNumber > 3 && lastNumber <= 6){
-       console.log(`${numbericColor} megahoms`);
-    }else if(lastNumber > 6 && lastNumber <= 9){
-       console.log(`${numbericColor} gigahoms`);
-    }else if(lastNumber >9 && lastNumber < 13){
-       console.log(`${numbericColor} terahoms`);
+   lastNumber = findLastNumber(tempListNumber);//4
+    console.log('lastNumber'+ lastNumber);
+   appendString = findAppendingStr(lastNumber);//00 00
+    console.log("apendString: "+ appendString);
+    console.log("numbericColor: "+ numbericColor);
+    appendString = numbericColor + appendString;//47 
 
+   
+    console.log("appendString: "+appendString);//470000
+    if( lastNumber === 3 || ( (Number(appendString) > 1000) && Number(appendString) < 10000 ) || ((Number(appendString) / 1000) < 1000)){
+      return `${Number(appendString)/1000} kiloohms`;
+    }else if(lastNumber > 3 || (Number(appendString) > 1000 && Number(appendString) < 10000000)){
+      return `${numbericColor} megaohms`; 
+    }else if(lastNumber > 6){
+      return `${numbericColor} gigaohms`; 
+    }else if(lastNumber >9 && lastNumber < 13){
+      return `${numbericColor} teraohms`; 
     }else{
-      console.log("not found");
+      if (Number(appendString) === 0) {
+        return  `${lastNumber} ohms`;
+      }else if(Number(appendString) / 1000 < 1000){
+        return `${( (Number(appendString)/ 1000 * 100) ) < 10 
+          ?  (Number(appendString)/ 1000 * 100) : 
+            (Number(appendString)/ 1000 * 100) * 10 } ohms`;
+      } 
+      return '';
     }
-    console.log(appendString);
-    console.log("---end---");
-  // 2. Killooms 1000
-  // 3. Megaohms 1000000
-  // 4. Gigaohms 1000000000
-  // 5. Teraohms 1000000000000
+}
+
+//Create a function to find out color name list [white,white,white] =>  [9, 9, 9]
+function findOutIndexNumeric(userColorInput: ColorTypes[] ): number[]{
+  //check size of userColorInput ex: [orange,orange,black] 330
+  let tempListNumeric: number[] = []; //store numberic temporary
+  userColorInput.length > 2 && 
+    userColorInput.map((colorName) => tempListNumeric.push( COLORS.indexOf(colorName)));
+  return tempListNumeric;
+}
+
+//Create a function to take out only two digits lieke [9,9,9] => [9,9]
+function findTwoDigit(listNumeric: number[]):string {
+  return listNumeric[0] === 0 ? listNumeric[1].toString() : listNumeric.join("");
+}
+
+//Find last number ex: [7,3,6] => [6]
+function findLastNumber(colorNumberic: number[]) : number {
+  //[1,2,3]
+  for(let i:number = 0; i < 2; i++){
+    colorNumberic.shift();
+  }
+  return colorNumberic[0];
+}
+
+//Find appending string. Ex:[4] -> 19 => 190000
+export function findAppendingStr(numeric: number): string {
+  switch(numeric){
+    case 2:
+      return countZero(2);
+      break;
+    case 3:
+      return countZero(3)
+      break;
+    case 4:
+      return countZero(4);
+      break;
+    case 5:
+      return countZero(5);
+      break;
+    case 6:
+      return countZero(6);
+      break;
+    case 7:
+      return countZero(7);
+      break;
+    case 8:
+      return countZero(8);
+      break;
+    case 9:
+      return countZero(9);
+      break;
+    default:
+      return "0";
+  }
+}
+
+function countZero(userCase: number): string{
+  let tempZeroList: number[] = [];
   
+  for(let i:number = 1; i <= userCase; i++){
+    tempZeroList.push(0);
+  };
+  
+  return tempZeroList.join("");
+ 
 }
