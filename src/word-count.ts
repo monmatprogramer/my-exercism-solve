0ab690e10f948cd7847e6f64d5bd148379888936
metracambodia <metracambodia@gmail.com>
2026-06-17 17:11:52 +0700

    add function to check apostro

--- src/word-count.ts
+++ src/word-count.ts
@@ -8,5 +8,15 @@ export function count(words:string):number{
 
 function convertToLowercase(words:string):string{
   const lc: string = words.toLocaleLowerCase();
+  findApostrophes(words);
   return lc;
 }
+
+// find apostrophes 
+function findApostrophes(message: string){
+  // Create Reg pattern
+  const fap = /\b[a-z0-9]+'?[a-z0-9]+\b|[a-z0-9]+\b/g;
+  const tr = fap.test(message);
+  console.log(tr);
+
+};
