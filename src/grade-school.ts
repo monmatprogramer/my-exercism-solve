e69c0daa277988b4195f773ce3c62ad4d26cf2dd
monmatprogramer <monsokmeanmon@gmail.com>
2026-06-29 17:09:44 +0700

    update code

--- src/grade-school.ts
+++ src/grade-school.ts
@@ -7,20 +7,24 @@ export class GradeSchool {
   // value: name
 
   //Private a list of the student
-  private static STUDENT_LIST: Map<number, string[]>; 
-  
+  private static STUDENT_LIST = new Map<number, string[]>(); 
+    
   roster() {
      
   }
 
-  add() {
+  add(stdName: string, score: number) {
+    //Check the score exist or not
+    if(!GradeSchool.STUDENT_LIST.has(score)){ // if score does not exist
+      //Add score
+      GradeSchool.STUDENT_LIST.set(score, [stdName]);
+      console.log(`Added success: ${GradeSchool.STUDENT_LIST.get(score)}`);
+    }
   }
 
   // return the list of students for one specific grade
   grade(score: number): string[] {
-    
-
     // Return copy array
-    return [...GradeSchool.STUDENT_LIST.get(3)];
+    return [];
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import {GradeSchool} from "./grade-school";
 
 const g = new GradeSchool();
+g.add("Mat", 100);
