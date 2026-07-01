b22bd26d33499e145d938b78ef29d23077384ea6
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-01 11:47:53 +0700

    Learn about converting binary and find remainder

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
-import {commands} from "./secret-handshake";
+//import {commands} from "./secret-handshake";
+
+const divide: number = 31;
+let result: number = 0;
+let tempDivide: number = divide;
+let remainder: number = 0;
 
 
-console.log(commands(3));
+do {
+  result = tempDivide / 2;
+  tempDivide = Math.floor(result);
+} while(tempDivide > 0);
 
+const listRemainder: number[] = [];
+//remainder
+tempDivide = divide;
+while (tempDivide > 0) {
+  listRemainder.push(divide % 2);
+  tempDivide = Math.floor(tempDivide / 2);
+  console.log(tempDivide);
+}
+
+console.log(listRemainder);
