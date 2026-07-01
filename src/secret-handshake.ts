5c0882a81caa5d32ba3bca0738f3762d50d8f3ff
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-01 13:58:39 +0700

    Create a function to check reverse or not

--- src/secret-handshake.ts
+++ src/secret-handshake.ts
@@ -4,7 +4,20 @@ const shm:string[] = ['wink', 'double blink', 'close your eyes','jump'];
 
 export function commands(input:number){
  const listBinary: number[] = convertToBinary(input); 
-  console.log(listBinary);
+ isReverse(listBinary);
+}
+function isReverse(lb: number[]): boolean{
+  //ture : reverse 
+  //false: not reverse
+  
+  //total length
+  const len: number = lb.length;
+
+  //first digit
+  const fd: number = lb[0];
+  const result: boolean = (len === 5) && (fd === 1) ? true : false;
+  console.log(result);
+  return result;
 }
 
 function convertToBinary(decimalNum: number): number[] {
