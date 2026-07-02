3df20c94fe6fe5c251b5112e36f2cccc869a33f8
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-02 16:58:49 +0700

    update

--- src/storage-manager.ts
+++ src/storage-manager.ts
@@ -1,8 +1,22 @@
 //This is storage manager challeng
 
 class MyStorageManager {
-  public uploadFile(){
+  public uploadFile(fileSize: number, availableSpace: number): string{
+    if(fileSize > availableSpace){
+      throw new Error('Upload failed: Not enough storage space!');
+    }
+    return 'File uploaded successfully!';
     
   }
 }
 
+
+const myStorageManager = new MyStorageManager();
+
+try{
+  const status:string = myStorageManager.uploadFile(3,3);
+
+  console.log(status);
+}catch(error: any){
+  console.log(error.message);
+}
