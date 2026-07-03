d403f95f4030d36af8172facd57f452d4033ef49
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-03 10:37:12 +0700

    update user login

--- src/User.ts
+++ src/User.ts
@@ -6,11 +6,23 @@ class User{
 
   constructor(username: string){
     this.userName = username;
-    this.isLogin = false;
+    this.isLogin = true;
   }
 
   login(){
+    if(this.isLogin === true){
+      throw new Error("User is already logged in");
+    }
     this.isLogin = true;
   }
+}
+
+const userLogin = new User('monmat');
+
+try{
+  userLogin.login();
+  console.log(userLogin.isLogin);
+}catch(error: any){
+  console.log(error.message);
 }
 
