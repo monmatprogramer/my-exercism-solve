012de60b61e8ecf63ff845bf004cf030ded7916b
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-03 15:13:08 +0700

    fix: change name of properites

--- src/split-second-stopwatch.ts
+++ src/split-second-stopwatch.ts
@@ -1,63 +1,72 @@
 export class SplitSecondStopwatch {
-  private state: string;
-  private currentLap: string;
-  private total: string;
-  private previousLaps: string[];
+  private _state: string;
+  private _currentLap: string;
+  private _total: string;
+  private _previousLaps: string[];
   
   constructor() {
-   this.state = 'ready';
-   this.currentLap = '00:00:00';
-   this.total = '00:00:00';
-   this.previousLaps = [];
+   this._state = 'ready';
+   this._currentLap = '00:00:00';
+   this._total = '00:00:00';
+   this._previousLaps = [];
   }
   // state stopwatch
   public get state(): string {
-    return this.state;
+    return this._state;
   }
 
   public get currentLap(): string {
-    return this.currentLap;
+    return this._currentLap;
   }
 
   public get total(): string {
-    return total;
+    return this._total;
   }
 
   public get previousLaps(): string[] {
-    return this.previousLaps;
+    return this._previousLaps;
   }
 
   // start stopwatch
   public start(): string {
-    if(this.state === 'running'){
+    if(this._state === 'running'){
       throw new Error('cannot start an already running stopwatch');
     }
-    this.state = 'running';
-    return this.state;
+    this._state = 'running';
+    return this._state;
   }
 
   public stop(): string {
-    if(this.state !== 'running'){
+    if(this._state !== 'running'){
       throw new Error('cannot stop a stopwatch that is not running');
     }
-    this.state = 'stopped';
-    return this.state;
+    this._state = 'stopped';
+    return this._state;
   }
 
-  public lap(): unknown {
-    if(this.state !== 'running'){
+  public lap(): string[] {
+    if(this._state !== 'running'){
       throw new Error('cannot lap a stopwatch that is not running');
     }
-    this.previousLaps.push(this.currentLap);
-    this.currentLap = '00:00:00';
-    return this.previousLaps;
+    this._previousLaps.push(this.currentLap);
+    this._currentLap = '00:00:00';
+    return this._previousLaps;
   }
 
-  public reset(): unknown {
-    throw new Error('Remove this line and implement the function')
+  public reset() {
+    if(this._state !== 'stopped'){
+      throw new Error('cannot reset a stopwatch that is not stopped');
+    }
+    this._state = 'ready';
+    this._currentLap = '00:00:00';
+    this._previousLaps = [];
   }
 
-  public advanceTime(duration: unknown): unknown {
-    throw new Error('Remove this line and implement the function')
+  public advanceTime(duration: string){
+    if(this._state !== 'running'){
+      throw new Error('cannot lap a stopwatch that is not running');
+    }
+    //calculate duration
+    this._total = duration;
   }
 }
