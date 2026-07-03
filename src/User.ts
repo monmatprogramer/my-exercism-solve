dd7f43ac101a35edeac91d55162be7f1a54739ad
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-03 11:10:04 +0700

    complete user login history

--- src/User.ts
+++ src/User.ts
@@ -2,19 +2,29 @@
 class User{
   userName:string;
   isLogin: boolean;
-
+  //add login history
+  loginHistory: string[];
 
   constructor(username: string){
     this.userName = username;
-    this.isLogin = true;
+    this.isLogin = false;
+    this.loginHistory = [];
   }
-
+  //Login
   login(){
     if(this.isLogin === true){
       throw new Error("User is already logged in");
     }
+    
     this.isLogin = true;
+    this.loginHistory.push('Logged in');
   }
+  //Logout
+  logout(){
+    this.isLogin = false;
+    this.loginHistory = [];
+  }
+
 }
 
 const userLogin = new User('monmat');
@@ -25,4 +35,6 @@ try{
 }catch(error: any){
   console.log(error.message);
 }
-
+console.log(userLogin.loginHistory);
+userLogin.logout();
+console.log(userLogin.loginHistory);
