5aa3782358049eb4f6e3cd55f1fa056b2c14cb9b
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-03 10:12:03 +0700

    Complete user login
    Signed-off-by: monmatprogramer <monsokmeanmon@gmail.com>

--- src/User.ts
+++ src/User.ts
@@ -1,2 +1,20 @@
 //User login
-//
+class User{
+  userName:string;
+  isLogin: boolean;
+
+
+  constructor(username: string){
+    this.userName = username;
+    this.isLogin = false;
+  }
+
+  login(){
+    this.isLogin = true;
+  }
+}
+
+const user = new User('monmat');
+
+user.login();
+console.log(user.isLogin);
