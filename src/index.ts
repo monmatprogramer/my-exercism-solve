70224bdcb372be943eaad7c77a4767e33517d82c
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-06 13:07:56 +0700

    try to fix lap()

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
 import {SplitSecondStopwatch} from "./split-second-stopwatch";
 
 const stopwatch = new SplitSecondStopwatch()
- try{
-   stopwatch.start()
-    stopwatch.advanceTime('00:00:22')
-    stopwatch.lap()
-    stopwatch.advanceTime('00:00:33')
-    console.log(stopwatch.total); // '00:00:55'
+ try{   
+
  }catch(er:any){
   console.log(er.message);
  }  
--- src/split-second-stopwatch.ts
+++ src/split-second-stopwatch.ts
@@ -3,12 +3,12 @@ export class SplitSecondStopwatch {
   private _currentLap: string;
   private _total: string;
   private _previousLaps: string[];
-  
+
   constructor() {
-   this._state = 'ready'; //ready
-   this._currentLap = '00:00:00';
-   this._total = '00:00:00';
-   this._previousLaps = [];
+    this._state = "ready"; //ready
+    this._currentLap = "00:00:00";
+    this._total = "00:00:00";
+    this._previousLaps = [];
   }
   // state stopwatch
   public get state(): string {
@@ -29,118 +29,115 @@ export class SplitSecondStopwatch {
 
   // start stopwatch
   public start(): string {
-    if(this._state === 'running'){
-      throw new Error('cannot start an already running stopwatch');
+    if (this._state === "running") {
+      throw new Error("cannot start an already running stopwatch");
     }
-    this._state = 'running';
+    this._state = "running";
     return this._state;
   }
 
   public stop(): string {
-    if(this._state !== 'running'){
-      throw new Error('cannot stop a stopwatch that is not running');
+    if (this._state !== "running") {
+      throw new Error("cannot stop a stopwatch that is not running");
     }
-    this._state = 'stopped';
+    this._state = "stopped";
     return this._state;
   }
 
   public lap() {
-    if(this._state !== 'running'){
-      throw new Error('cannot lap a stopwatch that is not running');
+    if (this._state !== "running") {
+      throw new Error("cannot lap a stopwatch that is not running");
     }
-    this._state = 'lapped';
-    this._currentLap = '';
+    this._state = "lapped";
+    this._currentLap = "";
   }
 
   public reset() {
-    if(this._state !== 'stopped'){
-      throw new Error('cannot reset a stopwatch that is not stopped');
+    if (this._state !== "stopped") {
+      throw new Error("cannot reset a stopwatch that is not stopped");
     }
-    this._state = 'ready';
-    this._currentLap = '00:00:00';
+    this._state = "ready";
+    this._currentLap = "00:00:00";
     this._previousLaps = [];
   }
 
-  public advanceTime(duration: string){
+  public advanceTime(duration: string) {
     //add advance time to be current lap and total
-    if(this._state === 'stopped'){
+    if (this._state === "stopped") {
       this._currentLap = duration;
       this._total = this._total;
-    }else if(this._state === 'lapped'){
+    } else if (this._state === "lapped") {
       this._previousLaps.push(duration);
-      this._total = totalAdvanceTime(this._previousLaps); 
+      this._total = totalAdvanceTime(this._previousLaps);
       this._currentLap = duration;
-      this._state = 'running';
+      this._state = "running";
       return this._previousLaps;
-    }else{
+    } else {
       this._currentLap = duration;
       // add current to _previousLaps
       this._previousLaps.push(this._currentLap);
       this._total = this._currentLap;
     }
     // it will got result as total second like 13
-    const totalSecond= totalPreviousLaps(this._previousLaps);
+    const totalSecond = totalPreviousLaps(this._previousLaps);
     // After that we convert total second to hr, mm, second.
     const hr: number = convertToHour(totalSecond);
     const mm: number = convertToMinute(totalSecond);
     const ss: number = convertToSecond(totalSecond);
     //conver them to string
-    const [shr,smm, sss]: string[] = [hr, mm, ss].map(String); 
-    this._total = `${shr.padStart(2,'0')}:${smm.padStart(2,'0')}:${sss.padStart(2,'0')}`;
+    const [shr, smm, sss]: string[] = [hr, mm, ss].map(String);
+    this._total = `${shr.padStart(2, "0")}:${smm.padStart(2, "0")}:${sss.padStart(2, "0")}`;
     this._currentLap = this._total;
   }
 }
 
 // Helper function to calculate stopwatch
 function calcTime(duration: string): number {
- const durationParts: string[] = duration.split(':');
- const [hour, minute, second] = durationParts.map(Number);
+  const durationParts: string[] = duration.split(":");
+  const [hour, minute, second] = durationParts.map(Number);
   // seconds
- const result: number =  hour * 3600 + minute * 60 + second;
-//it return total second for futur use
- return result;
+  const result: number = hour * 3600 + minute * 60 + second;
+  //it return total second for futur use
+  return result;
 }
 
-
 // Convert to hour
-function convertToHour(totalSeconds: number): number{
+function convertToHour(totalSeconds: number): number {
   const hour = Math.floor(totalSeconds / 3600);
   return hour;
 }
 
 // Convert to minute
-function convertToMinute(totalSeconds: number): number{
+function convertToMinute(totalSeconds: number): number {
   const minute = Math.floor((totalSeconds % 3600) / 60);
-  return minute; 
+  return minute;
 }
 
 // Convert to second
-function convertToSecond(totalSeconds: number): number{
+function convertToSecond(totalSeconds: number): number {
   const second: number = totalSeconds % 60;
   return second;
 }
 
-
-// Sum previousLaps 
-function totalPreviousLaps(previousLap: string[]){
-  let sumDuration: number = 0; 
+// Sum previousLaps
+function totalPreviousLaps(previousLap: string[]) {
+  let sumDuration: number = 0;
   let totalSecond: number = 0;
-  for(const [index, value] of previousLap.entries() ){
-    totalSecond = totalSecond + calcTime(value);  
+  for (const [index, value] of previousLap.entries()) {
+    totalSecond = totalSecond + calcTime(value);
   }
-  //it return back total second like 13 
+  //it return back total second like 13
   return totalSecond;
 }
 
-
 // totalAdvanceTime
-function totalAdvanceTime(previousLap: string[]): string{
+function totalAdvanceTime(previousLap: string[]): string {
   const totalSecond = totalPreviousLaps(previousLap);
   const hr: number = convertToHour(totalSecond);
   const mm: number = convertToMinute(totalSecond);
   const ss: number = convertToSecond(totalSecond);
   //conver them to string
-  const [shr,smm, sss]: string[] = [hr, mm, ss].map(String); 
-  const total = `${shr.padStart(2,'0')}:${smm.padStart(2,'0')}:${sss.padStart(2,'0')}`;
-  return total; 
+  const [shr, smm, sss]: string[] = [hr, mm, ss].map(String);
+  const total = `${shr.padStart(2, "0")}:${smm.padStart(2, "0")}:${sss.padStart(2, "0")}`;
+  return total;
 }
