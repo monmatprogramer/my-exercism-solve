f66c89713c208fadc09e665bc02c9dbcde2aa0ce
metracambodia <metracambodia@gmail.com>
2026-07-06 13:12:27 +0700

    for testing

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,12 @@ import {SplitSecondStopwatch} from "./split-second-stopwatch";
 
 const stopwatch = new SplitSecondStopwatch()
  try{   
-
+    stopwatch.start()
+stopwatch.advanceTime('00:08:22')
+stopwatch.lap()
+expect(stopwatch.currentLap).toBe('00:00:00')
+stopwatch.advanceTime('00:00:15')
+expect(stopwatch.currentLap).toBe('00:00:15')
  }catch(er:any){
   console.log(er.message);
  }  
