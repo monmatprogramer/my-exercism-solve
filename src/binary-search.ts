fec711258d6c2b11339926b28ae12e6c27e8b8a6
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-06 14:32:43 +0700

    Find the middle value

--- src/binary-search.ts
+++ src/binary-search.ts
@@ -1,1 +1,20 @@
 //Solving the binary search
+
+export function find(haystack: number[], needle: number): number {
+  findMiddle(haystack);
+  return 0;
+}
+// Findig middle
+function findMiddle(list: number[]): number {
+  //length of list
+  const len: number = list.length;
+  //Finding middle value
+  const middleValue: number = Math.floor(len / 2);
+  // find value in the middle list
+  const middleNum: number = list[middleValue];
+  console.log(middleNum);
+  return middleNum;
+}
+// Bigger than middle
+
+// Less than middle
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
-import { SplitSecondStopwatch } from "./split-second-stopwatch";
-
-const stopwatch = new SplitSecondStopwatch();
-try {
-  stopwatch.start();
-  stopwatch.advanceTime("00:11:22");
-  stopwatch.lap();
-  //expect(stopwatch.currentLap).toBe("00:00:00");
-  console.log(stopwatch.previousLaps);
-  stopwatch.stop();
-  //expect(stopwatch.currentLap).toBe("00:00:15");
-  console.log(stopwatch.previousLaps);
-} catch (er: any) {
-  console.log(er.message);
-}
+import { find } from "./binary-search";
+const haystack: number[] = [1, 3, 4, 6, 8, 9, 11];
+find(haystack, 3);
