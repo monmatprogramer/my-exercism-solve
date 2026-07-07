61aef5483e9d3f0bc97755605c08dbc6029b3181
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-07 14:55:06 +0700

    create base type

--- src/prism.ts
+++ src/prism.ts
@@ -1,17 +1,28 @@
 //Prism exercise
 //Create type aliase for 'start'
-type StartType = {
+//Create position
+// Base type
+type Position = {
   x: number;
   y: number;
   angle: number;
 };
-//Create type aliase for 'prisms'
-type PrismsType = StartType[];
+type StartType = Position;
 
+//Create type aliase for 'prisms'
+type PrismsType = Position & {
+  id: number;
+};
 export function findSequence(start: StartType, prisms: PrismsType): number[] {
+  // list of result of poiting
+  const result: number[] = [];
   //Check the light of sight
   for (const [index, value] of prisms.entries()) {
     calcuAnagle(start.x, value.x, start.y, value.y);
+    // find point match
+    if (calcuPointing(start.angle, value.angle)) {
+      return result.push(value.id);
+    }
   }
   return [0, 0];
 }
@@ -27,4 +38,12 @@ function calcuAnagle(x1: number, x2: number, y1: number, y2: number) {
   //Calculate
   angleResult = Math.atan2(y2 - y1, x2 - x1) * (180 / pi);
   console.log("calcuAnagle: ", angleResult);
+}
+
+// calculate pointing
+function calcuPointing(an1: number, an2: number): boolean {
+  if (an1 === an2) {
+    return true;
+  }
+  return false;
 }
