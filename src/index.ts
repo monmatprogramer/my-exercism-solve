c0467d669d927ddb22e32c162d01641372cc123b
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-07 17:29:24 +0700

    test2

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { findSequence } from "./prism";
-const start = { x: 0, y: 0, angle: 2340 };
+const start = { x: 0, y: 0, angle: 0 };
 const prisms1 = [
   { id: 1, x: 10, y: 0, angle: 0 },
   { id: 2, x: 0, y: 10, angle: 0 },
@@ -9,6 +9,10 @@ const prisms2 = [
   { id: 1, x: 10, y: 10, angle: 0 },
   { id: 2, x: 10, y: 0, angle: 90 },
 ];
-const prisms = [{ id: 1, x: 10, y: 0, angle: 0 }];
+const prisms11 = [{ id: 1, x: 10, y: 0, angle: 0 }];
+const prisms = [
+  { id: 2, x: 10, y: 0, angle: 0 },
+  { id: 1, x: 20, y: 0, angle: -180 },
+];
 
 console.log(findSequence(start, prisms));
