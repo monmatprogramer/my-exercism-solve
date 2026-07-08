6ea394920277e23c4e1db3edb80f9fbcf068b232
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-08 15:13:13 +0700

    change name properties

--- src/rational-numbers.ts
+++ src/rational-numbers.ts
@@ -1,24 +1,22 @@
 //rantonal number
 export class Rational {
-  private n: number;
-  private d: number;
+  numerator: number;
+  demominator: number;
   constructor(num: number, de: number) {
-    this.n = num;
-    this.d = de;
+    this.numerator = num;
+    this.demominator = de;
   }
 
   add(n2: Rational) {
     // Represent
-    const a: number = this.n;
-    const b: number = this.d;
-    const c: number = n2.n;
-    const d: number = n2.d;
+    const a: number = this.numerator;
+    const b: number = this.demominator;
+    const c: number = n2.numerator;
+    const d: number = n2.demominator;
     const numberator: number = a * d + c * b;
     const demominator: number = b * d;
     const result: number = numberator / demominator;
     const gcdResult: number = gcd(numberator, demominator);
-    console.log(numberator);
-    console.log(demominator);
     reduce(numberator, demominator, gcdResult);
   }
 
@@ -62,9 +60,6 @@ function gcd(numberator: number, demominator: number): number {
 function reduce(numberator: number, demominator: number, gcdResult: number) {
   const a: number = numberator / gcdResult;
   const b: number = demominator / gcdResult;
-  console.log(a);
-  console.log(b);
   const result: number = a / b;
-  console.log(result);
   return result;
 }
