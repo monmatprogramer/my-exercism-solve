9a0350e964c0bb92443134c4cb7d20c7e097d635
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-08 13:51:47 +0700

    create the function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-import { findSequence } from "./prism";
-const start = { x: 0, y: 0, angle: 0 };
-const prisms = [
-  { id: 2, x: 10, y: 0, angle: 0 },
-  { id: 1, x: 20, y: 0, angle: -180 },
-];
-console.log(findSequence(start, prisms));
+import { Rational } from "./rational-numbers";
+
+const actual = new Rational(1, 2).add(new Rational(2, 3));
--- src/rational-numbers.ts
+++ src/rational-numbers.ts
@@ -1,1 +1,31 @@
 //rantonal number
+export class Rational {
+  private n: number;
+  private d: number;
+  constructor(num: number, de: number) {
+    this.n = num;
+    this.d = de;
+  }
+
+  add(n2: Rational) {
+    calUreduceFraction(this.n, this.d, n2.n, n2.d);
+  }
+
+  sub() {}
+
+  mul() {}
+
+  div() {}
+
+  abs() {}
+
+  exprational() {}
+
+  expreal() {}
+
+  reduce() {}
+}
+
+// Helper function
+// Calculate unreduced fraction
+function calUreduceFraction(n1: number, d1: number, n2: number, d2: number) {}
