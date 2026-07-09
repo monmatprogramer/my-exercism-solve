a3c3a3f7484de04b80690ecdf59763c0e290d5ab
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-09 15:13:15 +0700

    optimized

--- src/rational-number-opti.ts
+++ src/rational-number-opti.ts
@@ -31,17 +31,11 @@ export class Rational {
     return new Rational(num, den);
   }
 
-  sub(n2: Rational): Rational {
-    const a: number = this.numerator;
-    const b: number = this.denominator;
-    const c: number = n2.numerator;
-    const d: number = n2.denominator;
-    const num: number = a * d - c * b;
-    const demom: number = b * d;
-    //Represent
-    this.numerator = num;
-    this.denominator = demom;
-    return this.reduce();
+  sub(other: Rational): Rational {
+    const num =
+      this.numerator * other.numerator - other.numerator * this.denominator;
+    const den = this.denominator * other.denominator;
+    return new Rational(num, den);
   }
 
   mul(n2: Rational): Rational {
