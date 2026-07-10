c3ce1551606f67379ddf4b3bce1995bac3188ed4
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-10 11:33:16 +0700

    update code

---
+++ src/learn-interface-type.ts
@@ -0,0 +1,19 @@
+//doc
+/*
+ * same basic job
+ *  blueprints for the data
+ *  it tells 'shapes of objects, functions, variable'
+ *
+ *
+ *
+ * */
+
+//----------interface-------
+/*
+ * it descripts shape of object
+ * */
+interface User {
+  id: number;
+  name: string;
+  isActive: boolean;
+}
