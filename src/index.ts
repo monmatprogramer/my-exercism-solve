021ed438878e438cf293d55939a11cb8cf45ff34
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-10 16:52:25 +0700

    simple cipher

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ import { SimpleCipher } from "./simple-cipher";
 const simpleCipher = new SimpleCipher();
 
 simpleCipher.decode();
+console.log(simpleCipher.keyCipher);
--- src/simple-cipher.ts
+++ src/simple-cipher.ts
@@ -1,4 +1,13 @@
 export class SimpleCipher {
+  keyCipher: string;
+  constructor(key?: string | undefined | null) {
+    if (key === "" || key) {
+      console.log("random");
+      this.keyCipher = "random";
+    } else {
+      this.keyCipher = "not random";
+    }
+  }
   encode() {
     console.log("encode");
   }
