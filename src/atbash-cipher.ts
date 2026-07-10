eed3738112bf883e91eb7abf379f20db328a0d92
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-10 13:42:29 +0700

    update code

--- src/atbash-cipher.ts
+++ src/atbash-cipher.ts
@@ -5,3 +5,13 @@ export function encode(plainText: string): string {
 export function decode(cipherText: string): string {
   return "decode fun";
 }
+
+// Helper function
+// Clearn text
+
+function cleanText(input: string): string {
+  // requla expression to accept only letter and number
+  const ex = /[^a-zA-Z0-9]/g;
+  //Cover to into lowercase
+  const lowCase: string = input.toLocaleLowerCase();
+}
