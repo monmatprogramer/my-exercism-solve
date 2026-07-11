4f1f4724f3401e67807b064135002d34a0ecb117
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-11 10:48:36 +0700

    finish generate key

--- src/simple-cipher.ts
+++ src/simple-cipher.ts
@@ -2,47 +2,7 @@ export class SimpleCipher {
   keyCipher: string;
   constructor(key?: string | undefined | null) {
     if (key === "" || key === undefined) {
-      // letter array here for do generating random key
-      const randomLetterArray: string[] = [
-        "a",
-        "b",
-        "c",
-        "d",
-        "e",
-        "f",
-        "g",
-        "h",
-        "i",
-        "j",
-        "k",
-        "l",
-        "m",
-        "n",
-        "o",
-        "p",
-        "q",
-        "r",
-        "s",
-        "t",
-        "u",
-        "v",
-        "w",
-        "x",
-        "y",
-        "z",
-      ];
-      let generatedKey: string;
-      let tempRandomKey: string[] = [];
-      //generate a random key
-      for (let i: number = 0; i < 100; i++) {
-        // run it 100 times
-        let indexedGenerated: number = Math.floor(Math.random() * 25); // for retreving a - z
-        // pickup a random letter from a - z
-        // Add the pickuped letter TO keyCipher
-        tempRandomKey.push(randomLetterArray[indexedGenerated]);
-      }
-
-      this.keyCipher = tempRandomKey.join("");
+      this.keyCipher = toTakeGeneratedKey();
     } else {
       // Save the key
       this.keyCipher = "not random";
@@ -54,4 +14,46 @@ export class SimpleCipher {
   decode() {
     console.log("decode");
   }
+}
+// Helper function to generate random key when the key is empty
+function toTakeGeneratedKey(): string {
+  // letter array here for do generating random key
+  const randomLetterArray: string[] = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  let tempRandomKey: string[] = [];
+  //generate a random key
+  for (let i: number = 0; i < 100; i++) {
+    // run it 100 times
+    let indexedGenerated: number = Math.floor(Math.random() * 25); // for retreving a - z
+    // pickup a random letter from a - z
+    // Add the pickuped letter TO keyCipher
+    tempRandomKey.push(randomLetterArray[indexedGenerated]);
+  }
+  return tempRandomKey.join("");
 }
