c4669880cd7c6bc3ebd1761a8ad67579b9de6216
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-11 09:41:28 +0700

    simple cipher

--- src/simple-cipher.ts
+++ src/simple-cipher.ts
@@ -2,8 +2,15 @@ export class SimpleCipher {
   keyCipher: string;
   constructor(key?: string | undefined | null) {
     if (key === "" || key === undefined) {
+      //generate a random key
+      for (let i: number = 1; i <= 100; i++) {
+        // run it 100 times
+        // pickup a random letter from a - z
+        // Add the pickuped letter TO keyCipher
+      }
       this.keyCipher = "random";
     } else {
+      // Save the key
       this.keyCipher = "not random";
     }
   }
