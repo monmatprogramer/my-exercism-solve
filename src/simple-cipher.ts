f73fa3de3030cf54645e3fa4a3bdf2929d49e700
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-11 10:26:26 +0700

    simple cipher

--- src/simple-cipher.ts
+++ src/simple-cipher.ts
@@ -2,9 +2,41 @@ export class SimpleCipher {
   keyCipher: string;
   constructor(key?: string | undefined | null) {
     if (key === "" || key === undefined) {
+      // letter array here for do generating random key
+      const randomLetterArray: string[] = [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+      ];
+      let generatedKey: string;
+
       //generate a random key
-      for (let i: number = 1; i <= 100; i++) {
+      for (let i: number = 0; i < 100; i++) {
         // run it 100 times
+        let indexedGenerated: number = Math.floor(Math.random() * 25); // for retreving a - z
         // pickup a random letter from a - z
         // Add the pickuped letter TO keyCipher
       }
