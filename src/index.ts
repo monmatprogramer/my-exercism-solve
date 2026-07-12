8016870165210d46384b61d37ab8f6f997ddc5cd
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-12 11:45:11 +0700

    update code

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { SimpleCipher } from "./simple-cipher";
 
 try {
   const simpleCipher = new SimpleCipher("abc");
-  console.log(simpleCipher.encode("iamapandabeaR"));
+  console.log(simpleCipher.decode("iboaqcnecbfcr"));
 } catch (e: any) {
   console.log(e.message);
 }
