89b20951dc671bbf3630114f08e358d28a7614b1
Mon Mat <monsokmeanmon@gmail.com>
2026-07-13 10:06:50 +0700

    clean string wordy

--- src/wordy.ts
+++ src/wordy.ts
@@ -5,9 +5,15 @@ export const answer = (question: string): number => {
 };
 
 const cleanString = (question: string): string => {
-  if (question === "") {
-    throw new Error("Error in cleanString function.");
-  } else {
-    return question;
+  //conver to lowercase
+  question = question.toLowerCase();
+  const toArray: string[] = question.split(" ");
+  if (
+    !(toArray[0] === "what") ||
+    !(toArray[1] === "is") ||
+    !toArray[toArray.length - 1].endsWith("?")
+  ) {
+    throw new Error("Unknown operation");
   }
+  return "";
 };
