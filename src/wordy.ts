fba871a5f4ed55a99a25c665b5be9a1357fe0a03
Mon Mat <monsokmeanmon@gmail.com>
2026-07-13 11:23:35 +0700

    clean string wordy

--- src/wordy.ts
+++ src/wordy.ts
@@ -8,9 +8,11 @@ const cleanString = (question: string): string => {
   //conver to lowercase
   question = question.toLowerCase();
   const toArray: string[] = question.split(" ");
+  //check empty array
   if (toArray.length < 0) {
     throw new Error("Syntax error");
   }
+  //not empty
   if (
     !(toArray[0] === "what") ||
     !(toArray[1] === "is") ||
@@ -18,5 +20,23 @@ const cleanString = (question: string): string => {
   ) {
     throw new Error("Unknown operation");
   }
+  simplify(toArray);
   return question;
+};
+const simplify = (validQuesion: string[]): string[] => {
+  //remove first element and second element
+  validQuesion.splice(0, 1);
+  validQuesion.splice(0, 1);
+  if (validQuesion.length === 1) {
+    const ex = /\?/;
+    let temp = validQuesion[0].replace(ex, "");
+    validQuesion.push(temp);
+  } else {
+    const ex = /\?/;
+    let temp: string = validQuesion.pop()!;
+    temp = temp.replace(ex, "");
+    validQuesion.push(temp);
+  }
+  console.log("validQuesion: ", validQuesion);
+  return validQuesion;
 };
