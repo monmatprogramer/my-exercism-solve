af7bb3b6d4977b622cc6fe7d0a4cb2685cf5da34
Mon Mat <monsokmeanmon@gmail.com>
2026-07-13 09:21:33 +0700

    clean string wordy

--- src/wordy.ts
+++ src/wordy.ts
@@ -1,4 +1,13 @@
 export const answer = (question: string): number => {
   console.log("question", question);
+  cleanString(question);
   return 0;
+};
+
+const cleanString = (question: string): string => {
+  if (question === "") {
+    throw new Error("Error in cleanString function.");
+  } else {
+    return question;
+  }
 };
