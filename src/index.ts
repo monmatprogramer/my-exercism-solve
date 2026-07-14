02eeff75b20757b5234c0cd2751226d214410f59
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-14 11:13:54 +0700

    wordy

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,3 @@
 import { List } from "./list-ops";
 
 List.create(1, 2, 3, 4);
-
-let userInput: unknown;
-userInput = {
-  id: 1,
-  name: "MonMat",
-  phone: "070836110",
-};
-
-if (typeof userInput === "string") {
-  console.log(userInput.toLocaleUpperCase());
-}
-
-if (typeof userInput === "number") {
-  console.log("number");
-}
-
-if (typeof userInput === "object") {
-  console.log("object");
-}
--- src/list-ops.ts
+++ src/list-ops.ts
@@ -1,12 +1,18 @@
 export class List {
   private items: unknown[];
+  constructor(...items: unknown[]) {
+    console.log(...items);
+    this.items = ["s"];
+    console.log("this.items: ", this.items);
+    console.log("constructor");
+  }
   public static create(...values: unknown[]): unknown {
     // Do *not* construct any array literal ([]) in your solution.
     // Do *not* construct any arrays through new Array in your solution.
     // DO *not* use any of the Array.prototype methods in your solution.
 
     // You may use the destructuring and spreading (...) syntax from Iterable.
-    console.log(values);
-    return undefined;
+    //console.log(values);
+    return new List(values);
   }
 }
