cac8209e2e2d229d7af7c55f72493a5c005ca49d
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-14 10:44:28 +0700

    wordy

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 import { List } from "./list-ops";
 
 List.create(1, 2, 3, 4);
+
+let userInput: unknown;
+userInput = "hello world";
+
+if (typeof userInput === "string") {
+  console.log(userInput.toLocaleUpperCase());
+}
