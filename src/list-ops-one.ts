797179e941666c9dc9eaceb0ef20c36d6ebd7af9
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-15 11:32:22 +0700

    day 02 update

--- src/list-ops-one.ts
+++ src/list-ops-one.ts
@@ -1,8 +1,13 @@
 export class List {
+  private items: unknown[] | number[];
+  //constructor
+  constructor(items: unknown[] | number[]) {
+    this.items = items;
+  }
   public static create<T>(
     ...values: unknown[] | number[]
   ): unknown[] | number[] {
-    return [...values];
+    return new List(values);
   }
   concatenate(listOfLists: unknown[] | number[]) {
     console.log(listOfLists);
@@ -16,4 +21,4 @@ const list2 = List.create(20, 12, 13, 14, 15, 16, 17, 18, 19, 11);
 const list3 = List.create(130, 21, 31, 41, 51, 61, 71, 81, 91, 40);
 const list4 = List.create(list2, list3);
 
-console.log(list4);
+list1.concatenate(list4);
