3060e3a4b4b27af850474bd17133af906bdad5d9
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-15 11:54:38 +0700

    day 02 update

--- src/list-ops-one.ts
+++ src/list-ops-one.ts
@@ -16,6 +16,3 @@ export class List<T> {
 const list1 = List.create(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 const list2 = List.create(20, 12, 13, 14, 15, 16, 17, 18, 19, 11);
 const list3 = List.create(130, 21, 31, 41, 51, 61, 71, 81, 91, 40);
-const list4 = List.create("string1", "string2", "string3", "string4");
-
-list1.concatenate(list2);
