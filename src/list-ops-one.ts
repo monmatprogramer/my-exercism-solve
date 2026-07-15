0ce8c77f6b78068c8e88d1c37890e5bbc1f8f8b6
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-15 11:20:42 +0700

    day 02 update

--- src/list-ops-one.ts
+++ src/list-ops-one.ts
@@ -1,10 +1,20 @@
 export class List {
-  public static create<T>(...values: number[]): number[] {
-    return [...values];
+  public static create<T>(...values: unknown[]): unknown[] {
+    console.log(typeof values);
+    if (typeof values === "number") {
+      return [...values];
+    } else if (typeof values === "object") {
+      return [0];
+    }
+    return [1];
   }
 }
 
 // ---call class---
 
 const list1 = List.create(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-console.log(list1);
+const list2 = List.create(20, 12, 13, 14, 15, 16, 17, 18, 19, 11);
+const list3 = List.create(130, 21, 31, 41, 51, 61, 71, 81, 91, 40);
+const list4 = List.create(list2, list3);
+
+console.log(list4);
