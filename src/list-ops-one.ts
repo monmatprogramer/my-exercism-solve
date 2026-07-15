e83087fdebb5e264aeecc3586a8d896a7b8dc67e
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-15 13:52:30 +0700

    update list ops one

--- src/list-ops-one.ts
+++ src/list-ops-one.ts
@@ -7,19 +7,19 @@ export class List<T> {
     // add
     return new List<T>(values);
   }
-  public concatenate(otherList: List<List<T>>): void {
+  public concatenate(otherList: List<List<T>>): T[] {
     const tempArray: T[] = [];
     for (const [index, value] of otherList.items.entries()) {
       for (const [jindex, jvalue] of value.items.entries()) {
         tempArray.push(jvalue);
       }
     }
-    console.log(tempArray);
+    return tempArray;
   }
 }
 
 // ---call class---
-
+let dis;
 const list1 = List.create(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 //const list2 = List.create(20, 12, 13, 14, 15, 16, 17, 18, 19, 11);
 //const list3 = List.create(130, 21, 31, 41, 51, 61, 71, 81, 91, 40);
@@ -29,4 +29,5 @@ const list1 = List.create(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 const t1 = List.create(23, 32);
 const t2 = List.create(34, 44);
 const t3 = List.create(t1, t2);
-list1.concatenate(t3);
+dis = list1.concatenate(t3);
+console.log(dis);
