34b80e0a811470bdeef755d8f0be54ac320c1e82
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-16 14:09:18 +0700

    revers string

--- src/reverse-string.ts
+++ src/reverse-string.ts
@@ -1,8 +1,20 @@
 // reverse string
-export function reverse(normalStr: string) {
-  if (normalStr === "") {
+export function reverse(normalStr: string): string {
+  const reversedStr: string[] = [];
+  if (normalStr.length < 0) {
     return "";
+  } else {
+    const normalStrArr: string[] = normalStr.split("");
+    const len: number = normalStrArr.length - 1;
+    console.log(len);
+    for (let i: number = 5; i <= 0; i--) {
+      console.log(normalStrArr[i]);
+      console.log(i);
+    }
+
+    console.log(reversedStr);
   }
+  return reversedStr.join("");
 }
 
 console.log(reverse("reward"));
