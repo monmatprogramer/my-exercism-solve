24ce94a222edc8060003f962f730bf8e4817b924
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-16 16:16:56 +0700

    triangle update

--- src/triangle.ts
+++ src/triangle.ts
@@ -17,6 +17,14 @@ export class Triangle {
   // at least two sides the same length
   get isIsosceles(): boolean {
     const sides: number[] = [this.sideA, this.sideB, this.sideC];
+    const tempSides: number[] = sides;
+    let count: number = 0;
+    let i: number = 0;
+    while (i < sides.length) {
+      let temLen: number = 0;
+      let temVal: number = tempSides.shift()!;
+      temLen = tempSides.filter((n) => n === temVal).length;
+    }
     return true;
   }
 
