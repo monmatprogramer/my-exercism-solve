62368e6e04114e47717e5e1a0cfbd2bc48ac4f5d
Mon Mat <monsokmeanmon@gmail.com>
2026-07-16 11:24:56 +0700

    square root update

--- src/square-root.ts
+++ src/square-root.ts
@@ -1,2 +1,8 @@
 //square root exercise
-export function squareRoot(radicand: unknown): unknown {}
+export function squareRoot(radicand: number): number {
+  let left: number = 1;
+  let right: number = radicand;
+  return 0;
+}
+
+console.log(squareRoot(25));
