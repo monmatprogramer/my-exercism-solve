3b1ec5a75249c68046a55467e8e2f0dfdd8afcc0
Mon Mat <monsokmeanmon@gmail.com>
2026-07-16 14:29:17 +0700

    triangle update

--- src/triangle.ts
+++ src/triangle.ts
@@ -1,1 +1,18 @@
 // triangle exercise
+export class Triangle {
+  constructor(...sides) {
+    throw new Error("Remove this line and implement the function");
+  }
+
+  get isEquilateral() {
+    throw new Error("Remove this line and implement the function");
+  }
+
+  get isIsosceles() {
+    throw new Error("Remove this line and implement the function");
+  }
+
+  get isScalene() {
+    throw new Error("Remove this line and implement the function");
+  }
+}
