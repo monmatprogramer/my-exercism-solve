118961f9e3641a1b3996c85d5b3242cda46884e3
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-17 09:13:46 +0700

    collatz conjecture update

---
+++ src/collatz-conjecture.ts
@@ -0,0 +1,7 @@
+// Collaz Conjecture
+// - start with number
+// - goal is reaching the number 1
+// n > 0 && not decimal (Only positive integers are allowed)
+export function steps(count: unknown): unknown {
+  throw new Error("Remove this line and implement the function");
+}
