6b50dad6b6664226f7ca41cd122efa91aea21dac
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-17 09:28:34 +0700

    collatz conjecture update

--- src/collatz-conjecture.ts
+++ src/collatz-conjecture.ts
@@ -3,9 +3,17 @@
 // - goal is reaching the number 1
 // n > 0 && not decimal (Only positive integers are allowed)
 export function steps(count: number): number {
+  let c: number = 0;
   // check bad inputs and throw error handling
   if (count <= 0 || !Number.isInteger(count)) {
     throw new Error("Only positive integers are allowed");
   }
+
   return 0;
+}
+
+try {
+  steps(-2);
+} catch (e: any) {
+  console.log(e.message);
 }
