d0186b0af4a17a0d115991a1b96663f2c38fec7a
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-17 08:47:02 +0700

    difference of squares update

--- src/difference-of-squares.ts
+++ src/difference-of-squares.ts
@@ -2,11 +2,24 @@ export class Squares {
   private _sumOfSquares: number;
   private _squareOfSum: number;
   private _difference: number;
-  constructor(count: number) {}
+  constructor(n: number) {
+    let sum: number = 0;
+    // 1 to n
+    for (let i: number = 1; i <= n; i++) {
+      sum = (n * (n + 1)) / 2;
+    }
+    this._squareOfSum = Math.pow(sum, 2);
+  }
 
-  get sumOfSquares(): number {}
+  get sumOfSquares(): number {
+    return 0;
+  }
 
-  get squareOfSum(): number {}
+  get squareOfSum(): number {
+    return 0;
+  }
 
-  get difference(): number {}
+  get difference(): number {
+    return 0;
+  }
 }
