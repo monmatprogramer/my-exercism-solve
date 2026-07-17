9bad853c0c4d0c433c26c1a4103f53ff7af33bb9
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-17 11:56:01 +0700

    etl update

--- src/etl.ts
+++ src/etl.ts
@@ -3,9 +3,22 @@
 type OldData = {
   [key: number]: string[];
 };
+
 type NewData = {
   [key: string]: number;
 };
-export function transform(oldData: OldData) {}
+export function transform(oldData: OldData) {
+  console.log(oldData);
+}
 
-console.log(JSON.stringify({ 1: ["A"] }));
+const old = {
+  1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+  2: ["D", "G"],
+  3: ["B", "C", "M", "P"],
+  4: ["F", "H", "V", "W", "Y"],
+  5: ["K"],
+  8: ["J", "X"],
+  10: ["Q", "Z"],
+};
+
+transform(old);
--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ const userRoles = new Map<number, string>();
 userRoles.set(101, "Admin");
 userRoles.set(102, "Moderator");
 
-console.log(JSON.stringify(userRoles));
+console.log(JSON.stringify(userRoles.get(101)));
