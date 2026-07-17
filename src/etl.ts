a3557437037ace500e15b662a267c01cb7921e84
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-17 14:33:52 +0700

    etl update

--- src/etl.ts
+++ src/etl.ts
@@ -30,9 +30,9 @@ for (const [scoreString, letterArray] of Object.entries(old)) {
   //Conver numerice to be number
   const scoreNumber = Number(scoreString);
   //loop on the array
-  console.log("👉: ", letterArray);
   for (const letter of letterArray) {
-    console.log("➡️: ", letter);
-    console.log("\n");
+    letterScores.set(letter, scoreNumber);
   }
 }
+
+console.log(letterScores);
