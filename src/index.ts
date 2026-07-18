1a274880d87c3ab289eb18f186a1280576f38281
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-18 12:00:42 +0700

    protein update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
-const userRoles = new Map<number, string>();
-
-userRoles.set(101, "Admin");
-userRoles.set(102, "Moderator");
-
-console.log(JSON.stringify(userRoles.get(101)));
+const num: number[] = [1, 2, 3];
+console.log(num.slice(1, 3));
