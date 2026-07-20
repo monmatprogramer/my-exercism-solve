e7272ef37a02f854bc251a9773fe41fcf68778da
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-20 14:35:33 +0700

    update

--- src/hamming.ts
+++ src/hamming.ts
@@ -3,6 +3,9 @@ export function compute(left: string, right: string): number {
   if (left.length !== right.length) {
     throw new Error("DNA strands must be of equal length.");
   }
+  for (const char of left) {
+    console.log(char);
+  }
   return 0;
 }
 
