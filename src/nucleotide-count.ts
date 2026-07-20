a97b62a8e0d5e1ddbf46b9130b2317933b59ddde
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-20 15:20:20 +0700

    update

--- src/nucleotide-count.ts
+++ src/nucleotide-count.ts
@@ -3,8 +3,17 @@ type DnaType = {
 };
 export function nucleotideCounts(input: string): DnaType {
   const dna: string = "ACGT";
+  let dnaList: DnaType = {
+    A: 0,
+    C: 0,
+    G: 0,
+    T: 0,
+  };
   for (const char of dna) {
-    console.log(char);
+    //Check input equal to char
+    if (input === char) {
+      // check what is dna letter
+    }
   }
   return {
     A: 0,
