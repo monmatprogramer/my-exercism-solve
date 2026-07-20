cedf3631da7ce085191321c265cebe1a873dc34c
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-20 14:33:30 +0700

    update

--- src/hamming.ts
+++ src/hamming.ts
@@ -1,3 +1,8 @@
-export function compute(left: unknown, right: unknown): unknown {
-  throw new Error("Remove this line and implement the function");
+export function compute(left: string, right: string): number {
+  if (left.length !== right.length) {
+    throw new Error("DNA strands must be of equal length.");
+  }
+  return 0;
 }
+
+console.log(compute("GGACTGAAATCTG", "GGACTGAAATCTG"));
