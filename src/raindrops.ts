4da147c054b9d0a8fd07322739ea62bf38320346
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-20 14:26:56 +0700

    update

--- src/raindrops.ts
+++ src/raindrops.ts
@@ -1,3 +1,20 @@
-export function convert() {
-  throw new Error("Remove this line and implement the function");
+export function convert(inputNumber: number): string {
+  let result: string = "";
+  if (inputNumber % 3 === 0) {
+    result = result + "Pling";
+  }
+  if (inputNumber % 5 === 0) {
+    result = result + "Plang";
+  }
+  if (inputNumber % 7 === 0) {
+    result = result + "Plong";
+  }
+
+  if (result.length === 0) {
+    return String(inputNumber);
+  } else {
+    return result;
+  }
 }
+
+console.log(convert(0));
