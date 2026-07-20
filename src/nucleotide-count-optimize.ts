83eb99204fa284b037c549498e6c7c86d98d2d12
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-20 16:50:20 +0700

    update

---
+++ src/nucleotide-count-optimize.ts
@@ -0,0 +1,9 @@
+// define exactly what keys are allowed
+export type DnaType = {
+  A: number;
+  C: number;
+  G: number;
+  T: number;
+};
+
+export function nucleotideCounts(input: string): DnaType {}
