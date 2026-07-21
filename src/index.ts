eb97cc0d3082165628997d1ceb2f7ea8e9b53af2
Mon Mat <monsokmeanmon@gmail.com>
2026-07-21 11:45:02 +0700

    update 2

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("A" === "a");
+type GameType = {
+  status: string;
+  cards: number;
+  tricks: number;
+};
+
+const test: GameType = {
+  status: "finished",
+  cards: 10,
+  tricks: 1,
+};
+console.log(test);
