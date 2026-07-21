1e07492f7d55c76e3f32e7800df613c2daa3b24d
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-21 13:32:26 +0700

    update

--- src/camicia.ts
+++ src/camicia.ts
@@ -4,15 +4,21 @@ type GameType = {
   cards: number;
   tricks: number;
 };
+type FaceCard = "J" | "Q" | "K" | "A";
 //Penalty formula
-type PenaltyType = {};
+const penaltyType: Record<FaceCard, number> = {
+  J: 1,
+  Q: 2,
+  K: 3,
+  A: 4,
+};
 export const simulateGame = (
   playerA: string[],
   playerB: string[],
 ): GameType => {
   const pile: string[] = [];
   let [totalCards, totalTricks] = [0, 0];
-  let currentTurn: string = "A";
+  let currentTurn: FaceCard = "A";
   return {
     status: "finished",
     cards: 10,
