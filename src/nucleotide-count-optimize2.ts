d61f1bb50e10198b8f72edcfd90d4fffc8f8d255
Mon Mat <monsokmeanmon@gmail.com>
2026-07-21 10:48:06 +0700

    update 2

--- src/nucleotide-count-optimize2.ts
+++ src/nucleotide-count-optimize2.ts
@@ -16,12 +16,17 @@ export function nucleotideCounts(input: string): DnaType {
   };
   //Covert input string into an array
   const inputArray: string[] = input.split("");
-  inputArray.reduce((counts, char) => {
-    console.log("count: ", counts);
-    console.log("char: ", char);
-    console.log("\n");
-    return "";
-  });
+  inputArray.reduce(
+    (counts, char) => {
+      if (char in counts) {
+        console.log("👉 ", counts);
+        counts[char as keyof DnaType]++;
+        return counts;
+      }
+      throw new Error("Invalid nucleotideCounts");
+    },
+    { A: 0, C: 0, G: 0, T: 0 },
+  );
 
   return dna;
 }
