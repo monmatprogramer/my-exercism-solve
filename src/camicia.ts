9f79f97b35f44ce72bc748ec2579827c15a591bf
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-21 11:59:31 +0700

    update

--- src/camicia.ts
+++ src/camicia.ts
@@ -4,11 +4,15 @@ type GameType = {
   cards: number;
   tricks: number;
 };
+//Penalty formula
+type PenaltyType = {};
 export const simulateGame = (
   playerA: string[],
   playerB: string[],
 ): GameType => {
   const pile: string[] = [];
+  let [totalCards, totalTricks] = [0, 0];
+  let currentTurn: string = "A";
   return {
     status: "finished",
     cards: 10,
