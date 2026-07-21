dd941fb9ea8befca9d6cdf3b2800e08a8e13d52b
Mon Mat <monsokmeanmon@gmail.com>
2026-07-21 09:36:01 +0700

    update 2

--- src/learn-keyof.ts
+++ src/learn-keyof.ts
@@ -19,5 +19,12 @@ type Server = {
 };
 //AllowedKeys = 'model' | 'memory' | 'isOnline'
 type AllowedKeys = keyof Server;
+// 1. we create our data
+const myNAS = {
+  model: "QNAP TS-433",
+  memory: "4G",
+  isOnline: true,
+};
 // -- Common Use Case ---
 // 1. Type-Safe Dynamic Property Access
+function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {}
