0e7688c6b156fc404db4c571f4375650cd1bc10a
Mon Mat <monsokmeanmon@gmail.com>
2026-07-21 13:16:08 +0700

    update 2

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
-type GameType = {
-  status: string;
-  cards: number;
-  tricks: number;
+const obj: Record<string, number> = {
+  J: 1,
+  Q: 2,
+  K: 3,
+  A: 4,
 };
 
-const test: GameType = {
-  status: "finished",
-  cards: 10,
-  tricks: 1,
-};
-console.log(test);
+console.log(obj);
