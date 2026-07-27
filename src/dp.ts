45b2f88f983da7532614237bb755a86a3c100ea9
Mon Mat <monsokmeanmon@gmail.com>
2026-07-27 11:18:26 +0700

    update

--- src/dp.ts
+++ src/dp.ts
@@ -2,7 +2,9 @@
 
 const target = 5;
 
-let memory = new Array(6).fill(undefined);
+let memory: (number[] | undefined)[] = new Array(6).fill(undefined);
+
+memory[3] = [1, 2];
 
 // There are 6 slots (0-5)
 console.log(memory);
