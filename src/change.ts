9d3e0d167e15e9c1fa866a9ccf71aa8fcd749f4b
Mon Mat <monsokmeanmon@gmail.com>
2026-07-27 10:33:43 +0700

    update

--- src/change.ts
+++ src/change.ts
@@ -1,6 +1,14 @@
-export const findFewestCoins = (
-  coins: number[],
-  target: number,
-): number[] => {};
+export const findFewestCoins = (coins: number[], target: number): number[] => {
+  const bestSolution: number[] = [];
+  if (target < 0) {
+    throw new Error("It is negative");
+  }
+  if (target === 0) {
+    return [];
+  }
+
+  for (let i: number = 0; i < target; i++) {}
+  return [];
+};
 
 findFewestCoins([1, 5, 10, 25], 1);
