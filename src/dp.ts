3386310fdfa64f8ae4bc96c3ea93347bf246d32d
Mon Mat <monsokmeanmon@gmail.com>
2026-07-27 11:35:09 +0700

    update

--- src/dp.ts
+++ src/dp.ts
@@ -5,7 +5,10 @@ const target = 4;
 let memory: (number[] | undefined)[] = new Array(target + 1).fill(undefined);
 
 function findStudentScore() {
-  let scores: (number | undefined)[] = new Array(5);
+  // we create 5 slotes to store score
+  let scores: (number | undefined)[] = new Array(5).fill(undefined);
+  //save scores
+  scores[2] = 95;
   console.log("scores array: ", scores);
 }
 
