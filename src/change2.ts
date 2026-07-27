cfa985548c0df05ee9ac65ce981205aa4ddb2a22
Mon Mat <monsokmeanmon@gmail.com>
2026-07-27 11:15:21 +0700

    update

--- src/change2.ts
+++ src/change2.ts
@@ -1,4 +1,10 @@
 function findFewesCoins(coins: number[], target: number): number[] {
   if (target < 0) throw new Error("Target cann't be negative");
   if (target === 0) return [];
+  let memory: (number[] | undefined)[] = new Array(target + 1).fill(undefined);
+  memory[0] = [];
+  console.log(memory);
+  return [];
 }
+
+findFewesCoins([1, 5, 10, 25, 100], 15);
---
+++ src/dp.ts
@@ -0,0 +1,5 @@
+// Dynamic Programming (DP) or Memoization
+
+const target = 5;
+
+let memory = new Array(6).fill(undefined);
