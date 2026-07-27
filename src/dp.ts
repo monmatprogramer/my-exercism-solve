13f1751bfa43a08a8bf620e505ec44f3672650ef
Mon Mat <monsokmeanmon@gmail.com>
2026-07-27 11:22:16 +0700

    update

--- src/dp.ts
+++ src/dp.ts
@@ -1,8 +1,8 @@
 // Dynamic Programming (DP) or Memoization
 
-const target = 5;
+const target = 4;
 
-let memory: (number[] | undefined)[] = new Array(6).fill(undefined);
+let memory: (number[] | undefined)[] = new Array(target + 1).fill(undefined);
 
 memory[3] = [1, 2];
 
