0490ed259132ef29e4aea56dcef8c3d734151b6f
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-28 14:33:20 +0700

    update

--- src/learn-arr.ts
+++ src/learn-arr.ts
@@ -1,30 +1,5 @@
-//Learn array
-const input: string = "one car two car three car three dog four car";
-let notFoundWord: string [] = input.split(" ");
-
-console.log(notFoundWord.indexOf('one'));
-
-/*
- *Document 
- *  ## Remove specific index of array
- *  use splice(index, number of element want to remove)
- * */
-
-const value: string = notFoundWord[0];
-const two: string = notFoundWord[1];
-const three: string = notFoundWord[2];
-const myMap = new Map<string,number>();
-const match: string[] = notFoundWord.filter((v) => v === value);
-const matchTwo: string[] = notFoundWord.filter((v) => v === two);
-const matchThree: string[] = notFoundWord.filter((v) => v === three);
-let result: string[] = [];
-for(let i:number = 0; i < notFoundWord.length; i++){
-  let value: string = notFoundWord[i];
-  //index: 0, 1
-  result = notFoundWord.filter((v) => v === value );
-  myMap.set(value, result.length); 
+// Learn 2 dimension
+function twoDimensionArray() {
+  const rowNum: number = 2;
+  const rowCol: number = 3;
 }
-
-
-console.log(input);
-console.log(myMap);
