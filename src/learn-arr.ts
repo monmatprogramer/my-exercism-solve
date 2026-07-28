7a6d8c67b4774c1cecf84b655925b7ab89dd64e5
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-28 14:40:15 +0700

    update

--- src/learn-arr.ts
+++ src/learn-arr.ts
@@ -2,4 +2,8 @@
 function twoDimensionArray() {
   const rowNum: number = 2;
   const rowCol: number = 3;
+  const grid: (number | undefined)[][] = Array.from({ length: 2 });
+  console.log(grid);
 }
+
+twoDimensionArray();
