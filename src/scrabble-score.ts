67170b85ccbefe29f1cc7b662487de6c92d7590c
Mon Mat <monsokmeanmon@gmail.com>
2026-07-29 11:13:59 +0700

    update

--- src/scrabble-score.ts
+++ src/scrabble-score.ts
@@ -1,19 +1,30 @@
 //scrabble score exercise
-// Create unique type for word (user input 999999999999999999)
-export function score(word: string | undefined | null = null): number {
+// Create unique type for word (user input)
+type WordType = string | undefined | null;
+export function score(word?: WordType): number {
   //create empty totalScore
   let totalScore: number = 0;
   // Create letterUpperCase
   let letterUpperCase: string = "";
+  // Create a valid validWord
+  let validWord: string = "";
+  // Check condition word
   if (word === "" || word === undefined) {
     return 0;
   }
+  // Check validation of word
+  isValid(word);
   //Check letter to uppercase
-  letterUpperCase = toUpperCase(word);
+  //letterUpperCase = toUpperCase(word);
 
   return 0;
 }
+// Check validation
+function isValid(rawWord: WordType): string {
+  const wordRex = /^[a-zA-Z]/;
+  return "invalid";
+}
 // Create a function to change each letters to upper case
-function toUpperCase(rawWord: string | undefined | null = null): string {
+function toUpperCase(rawWord: string): string {
   return "";
 }
