48c3fa1b1130447472b605791f186c19fb1fba75
Mon Mat <monsokmeanmon@gmail.com>
2026-07-29 17:14:30 +0700

    update

---
+++ src/learn_use_letterScore.ts
@@ -0,0 +1,7 @@
+// This is learn to use letter score from scrabble exercise
+// Learn to use it
+
+import { letterScores } from "./scrabble-score";
+
+const scoreForZ = letterScores["Z"];
+console.log(scoreForZ);
--- src/scrabble-score.ts
+++ src/scrabble-score.ts
@@ -1,5 +1,40 @@
 //scrabble score exercise
-type letterScoreMap = Record<string, number>;
+type LetterScoreMap = Record<string, number>;
+export const letterScores: LetterScoreMap = {
+  // Value 1
+  A: 1,
+  E: 1,
+  I: 1,
+  O: 1,
+  U: 1,
+  L: 1,
+  N: 1,
+  R: 1,
+  S: 1,
+  T: 1,
+  // Value 2
+  D: 2,
+  G: 2,
+  // Value 3
+  B: 3,
+  C: 3,
+  M: 3,
+  P: 3,
+  // Value 4
+  F: 4,
+  H: 4,
+  V: 4,
+  W: 4,
+  Y: 4,
+  // Value 5
+  K: 5,
+  // Value 8
+  J: 8,
+  X: 8,
+  // Value 10
+  Q: 10,
+  Z: 10,
+};
 // Create unique type for word (user input)
 type WordType = string | undefined | null;
 export function score(word?: WordType): number {
