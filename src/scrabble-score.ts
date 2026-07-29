720c0e806b4272da173c2ba6a0a04da74aee4695
Mon Mat <monsokmeanmon@gmail.com>
2026-07-29 10:15:47 +0700

    update

--- src/scrabble-score.ts
+++ src/scrabble-score.ts
@@ -1,6 +1,8 @@
 //scrabble score exercise
 
 export function score(word: string | undefined | null = null): number {
+  //create empty totalScore
+  let totalScore: number = 0;
   if (word === "" || word === undefined) return 0;
   return 0;
 }
