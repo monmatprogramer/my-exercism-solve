8d7880a344fe3c698a3c2a964ccb2f2445c8bda7
Mon Mat <monsokmeanmon@gmail.com>
2026-07-29 17:18:41 +0700

    update

--- src/learn_use_letterScore.ts
+++ src/learn_use_letterScore.ts
@@ -2,6 +2,13 @@
 // Learn to use it
 
 import { letterScores } from "./scrabble-score";
-
+// {'Z':10}
 const scoreForZ = letterScores["Z"];
-console.log(scoreForZ);
+//console.log(scoreForZ);
+const word: string = "quirky";
+const wordUpper: string = word.toUpperCase();
+
+// find word in letter scores
+for (const char of wordUpper) {
+  console.log(char);
+}
