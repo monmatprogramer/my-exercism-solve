779baa2a1896042d49e650a796b3d2d0ab5056ec
Mon Mat <monsokmeanmon@gmail.com>
2026-07-29 10:21:28 +0700

    update

--- src/scrabble-score.ts
+++ src/scrabble-score.ts
@@ -1,15 +1,19 @@
 //scrabble score exercise
 
-export function score(word: string | undefined | null = null): number {
+export function score(word: string | undefined): number {
   //create empty totalScore
   let totalScore: number = 0;
   // Create letterUpperCase
   let letterUpperCase: string = "";
-  if (word === "" || word === undefined) return 0;
+  if (word === "" || word === undefined) {
+    return 0;
+  }
   //Check letter to uppercase
+  letterUpperCase = toUpperCase(word);
+
   return 0;
 }
 // Create a function to change each letters to upper case
-function toUpperCase(rawWord: string): string {
+function toUpperCase(rawWord: string | undefined): string {
   return "";
 }
