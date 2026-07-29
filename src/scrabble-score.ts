1c53a4de30cd0cf5286a8d88918296dcbdfc5552
Mon Mat <monsokmeanmon@gmail.com>
2026-07-29 10:18:24 +0700

    update

--- src/scrabble-score.ts
+++ src/scrabble-score.ts
@@ -3,6 +3,9 @@
 export function score(word: string | undefined | null = null): number {
   //create empty totalScore
   let totalScore: number = 0;
+  // Create letterUpperCase
+  let letterUpperCase: string = "";
   if (word === "" || word === undefined) return 0;
+  //Check letter to uppercase
   return 0;
 }
