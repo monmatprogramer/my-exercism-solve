65323576abaf64794f71d86808b1025f43a92605
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-30 16:00:58 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { valid } from "./luhn";
 
 let d: string = "12 345 334 34";
-valid(d);
+
+console.log(valid("1"));
--- src/luhn.ts
+++ src/luhn.ts
@@ -3,6 +3,7 @@ export function valid(digitString: string): boolean {
   // remove  all space from input string
   digitString = digitString.trim().replaceAll(" ", "");
   // check length of digitString
+  return digitString.length < 2 && false;
   console.log(digitString);
   return false;
 }
