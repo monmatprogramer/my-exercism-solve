3a00ead34c95194aa2577ad9245796c4342b68e6
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-30 09:38:02 +0700

    update

---
+++ src/allergies.ts
@@ -0,0 +1,13 @@
+export class Allergies {
+  constructor(allergenIndex: unknown) {
+    throw new Error("Remove this line and implement the function");
+  }
+
+  public list(): unknown {
+    throw new Error("Remove this line and implement the function");
+  }
+
+  public allergicTo(allergen: unknown): unknown {
+    throw new Error("Remove this line and implement the function");
+  }
+}
