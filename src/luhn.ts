81b23318bb3ad06c22c05503fdf60dd5a9f59971
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-30 15:53:42 +0700

    update

--- src/luhn.ts
+++ src/luhn.ts
@@ -2,6 +2,7 @@
 export function valid(digitString: string): boolean {
   // remove  all space from input string
   digitString = digitString.trim().replaceAll(" ", "");
+  // check length of digitString
   console.log(digitString);
   return false;
 }
