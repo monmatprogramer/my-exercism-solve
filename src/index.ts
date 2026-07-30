bbd630dee6bab78156b21acc917dc3508bb93ee9
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-30 16:06:15 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ import { valid } from "./luhn";
 
 let d: string = "12 345 334 34";
 
-console.log(valid("1"));
+//console.log(valid("111"));
+
+const getNumOnlyRex = /^[0-1]+$/;
+
+console.log(getNumOnlyRex.test("11a11"));
--- src/luhn.ts
+++ src/luhn.ts
@@ -1,9 +1,11 @@
 //Luhn exercise
 export function valid(digitString: string): boolean {
+  const getNumOnlyRex = /^[0-1]+$/;
   // remove  all space from input string
   digitString = digitString.trim().replaceAll(" ", "");
   // check length of digitString
   return digitString.length < 2 && false;
+  return getNumOnlyRex.test(digitString) === false && false;
   console.log(digitString);
   return false;
 }
