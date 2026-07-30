49799607aed63e62fad6848e8d1deb24e28952eb
Mon Mat <monsokmeanmon@gmail.com>
2026-07-30 18:54:31 +0700

    update

--- src/luhn.ts
+++ src/luhn.ts
@@ -2,7 +2,7 @@
 export function valid(digitString: string): boolean {
   const getNumOnlyRex = /^[0-9]+$/;
   //condition 1:  remove  all space from input string
-  digitString = digitString.trim().replaceAll(" ", "");
+  digitString = digitString.trim().replace(" ", "");
   //condition 2:  check length of digitString
   if (digitString.length < 2) return false;
   //condition 3:
@@ -25,7 +25,6 @@ export function valid(digitString: string): boolean {
     isSecondDigit = !isSecondDigit;
   }
 
-  console.log("total_sum: ", totalSum);
   if (totalSum % 10 === 0) {
     return true;
   } else {
