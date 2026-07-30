15dadfe8f12a5676fa5ef677c5484da9bda96460
Mon Mat <monsokmeanmon@gmail.com>
2026-07-30 19:09:40 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import { valid } from "./luhn";
 
 let d: string = "12 345 334 34";
 
-console.log(valid("59"));
+console.log(valid("059"));
--- src/luhn.ts
+++ src/luhn.ts
@@ -10,15 +10,13 @@ export function valid(digitString: string): boolean {
 
   let totalSum: number = 0;
   let isSecondDigit: boolean = false;
-  console.log("📝: ", digitString);
   const digitStringArr: string[] = digitString.split("");
-  console.log("👉: ", digitStringArr);
   const digitNumberArr: number[] = digitStringArr.map(Number);
-  console.log("➡️: ", digitNumberArr);
   let tempNumber: number = 0;
   for (let i: number = digitNumberArr.length - 1; i >= 0; i--) {
     isSecondDigit = i % 3 === 1;
     if (isSecondDigit) {
+      console.log("🔍: ", digitNumberArr[i]);
       tempNumber = digitNumberArr[i] * 2;
       if (tempNumber > 9) {
         tempNumber = tempNumber - 9;
