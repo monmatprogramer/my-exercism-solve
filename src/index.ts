ce27d26ef933d99185de37a3414a26fd5e3614e3
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-30 16:11:11 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ import { valid } from "./luhn";
 
 let d: string = "12 345 334 34";
 
-console.log(valid("111"));
+//console.log(valid("111"));
+const a = 2;
+const b = 3;
+
+console.log(a < b && "Hello");
--- src/luhn.ts
+++ src/luhn.ts
@@ -4,7 +4,7 @@ export function valid(digitString: string): boolean {
   // remove  all space from input string
   digitString = digitString.trim().replaceAll(" ", "");
   // check length of digitString
-  console.log("➡️ ", digitString);
+  console.log("➡️ ", digitString.length);
   return digitString.length < 2 && false;
   console.log("👉 ", digitString);
   return getNumOnlyRex.test(digitString) === false && false;
