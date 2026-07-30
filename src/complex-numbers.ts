7afc5848b89fbd93a2a1445fe653ea6ad47c8a41
Mon Mat <monsokmeanmon@gmail.com>
2026-07-30 15:05:50 +0700

    update

--- src/complex-numbers.ts
+++ src/complex-numbers.ts
@@ -28,7 +28,9 @@ export class ComplexNumber {
   }
 
   public div(other: ComplexNumber): ComplexNumber {
-    const newReal: number = 0;
+    const newReal: number =
+      (this.a * other.real + this.b * other.imag) /
+      (Math.pow(other.real, 2) + Math.pow(other.imag, 2));
     const newImag: number = 0;
     return new ComplexNumber(newReal, newImag);
   }
