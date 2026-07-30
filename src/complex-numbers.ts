90a13e8519e80ef793127543119037b9e46aaba1
Mon Mat <monsokmeanmon@gmail.com>
2026-07-30 14:15:48 +0700

    update

--- src/complex-numbers.ts
+++ src/complex-numbers.ts
@@ -1,41 +1,45 @@
 export class ComplexNumber {
-  constructor(real: unknown, imaginary: unknown) {
-    throw new Error("Remove this line and implement the function");
+  private a: number;
+  private b: number;
+
+  constructor(real: number, imaginary: number) {
+    this.a = real;
+    this.b = imaginary;
   }
 
   public get real(): number {
-    throw new Error("Remove this line and implement the function");
+    return 0;
   }
 
   public get imag(): number {
-    throw new Error("Remove this line and implement the function");
+    return 0;
   }
 
-  public add(other: unknown): unknown {
-    throw new Error("Remove this line and implement the function");
+  public add(other: ComplexNumber): ComplexNumber {
+    return other;
   }
 
-  public sub(other: unknown): unknown {
-    throw new Error("Remove this line and implement the function");
+  public sub(other: ComplexNumber): ComplexNumber {
+    return other;
   }
 
-  public div(other: unknown): unknown {
-    throw new Error("Remove this line and implement the function");
+  public div(other: ComplexNumber): ComplexNumber {
+    return other;
   }
 
-  public mul(other: unknown): unknown {
-    throw new Error("Remove this line and implement the function");
+  public mul(other: ComplexNumber): ComplexNumber {
+    return other;
   }
 
-  public get abs(): unknown {
-    throw new Error("Remove this line and implement the function");
+  public get abs(): number {
+    return 0;
   }
 
-  public get conj(): unknown {
-    throw new Error("Remove this line and implement the function");
+  public get conj(): ComplexNumber {
+    return new ComplexNumber(0, 0);
   }
 
   public get exp(): ComplexNumber {
-    throw new Error("Remove this line and implement the function");
+    return new ComplexNumber(0, 0);
   }
 }
