09d92714d001d9ead2131a012fd70237a8f44a4e
monmatprogramer <monsokmeanmon@gmail.com>
2026-07-30 16:11:19 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,4 @@ import { valid } from "./luhn";
 
 let d: string = "12 345 334 34";
 
-//console.log(valid("111"));
-const a = 2;
-const b = 3;
-
-console.log(a < b && "Hello");
+console.log(valid("111"));
