f17d5ac1625f8691df55e14009c7e176274fb1d2
Mon Mat <monsokmeanmon@gmail.com>
2026-07-31 10:01:45 +0700

    update

---
+++ src/grain.ts
@@ -0,0 +1,8 @@
+//grain exercise
+export const square = () => {
+  console.log("square");
+};
+
+export const total = () => {
+  console.log("total");
+};
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-import { valid } from "./luhn";
-
-let d: string = "055 444 285";
-
-console.log(valid(d));
-//d = d.trim().split(" ").join("");
-//console.log(d);
+import { square, total } from "./grain";
+square();
+total();
