89378edde3d93c930f996e6e6e57944591cb8210
Mon Mat <monsokmeanmon@gmail.com>
2026-07-31 11:01:18 +0700

    update

---
+++ src/pythagorean-triplet.ts
@@ -0,0 +1,19 @@
+type Options = {
+  minFactor?: number;
+  maxFactor?: number;
+  sum: number;
+};
+
+export function triplets({}: Options): Triplet[] {
+  throw new Error("Remove this line and implement the function");
+}
+
+class Triplet {
+  constructor() {
+    throw new Error("Remove this line and implement the function");
+  }
+
+  toArray(): [number, number, number] {
+    throw new Error("Remove this line and implement the function");
+  }
+}
