1f3da6e6054bae7d7d6226d83b2f97030da1874f
Mon Mat <monsokmeanmon@gmail.com>
2026-07-31 16:42:13 +0700

    Learn graph update

--- src/learn-graph.ts
+++ src/learn-graph.ts
@@ -1,1 +1,8 @@
 // We learn about graph algorithm which cause our system easy to search
+
+const networkMap = new Map<string, Set<string>>();
+
+const configFile = {
+  Vera: ["Tomoko"],
+  Tomoko: ["Aditi"],
+};
