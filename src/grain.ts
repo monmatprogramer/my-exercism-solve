5c8bef901e239aa4a0cd1143b6d719b007663f0f
Mon Mat <monsokmeanmon@gmail.com>
2026-07-31 10:44:47 +0700

    update

--- src/grain.ts
+++ src/grain.ts
@@ -1,9 +1,10 @@
 //grain exercise
-export const square = (N: number) => {
+export const square = (N: number): BigInt => {
   if (N < 1 || N > 64) {
     throw new Error("Square must be between 1 and 64");
   }
-  console.log("square");
+  const bigN: BigInt = BigInt(N);
+  return 2n ** (bigN - 1n);
 };
 
 export const total = () => {
