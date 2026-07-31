378e941509170b48c8a0ce95f80df2c981d7a9de
Mon Mat <monsokmeanmon@gmail.com>
2026-07-31 13:16:25 +0700

    update

--- src/learn-object-param.ts
+++ src/learn-object-param.ts
@@ -61,4 +61,4 @@ function createButton(props: ButtonProps) {
   console.log("Creating " + props.color + " button: " + props.label);
   console.log("Is it disable? " + disabledSdtate);
 }
-createButton({ label: "Save", color: "Blue", isDisable: true });
+//createButton({ label: "Save", color: "Blue", isDisable: true });
