0815657a4cffe28bebd191abf4dd6fbfd8018533
Mon Mat <monsokmeanmon@gmail.com>
2026-07-31 10:30:20 +0700

    update

--- src/grain.ts
+++ src/grain.ts
@@ -1,5 +1,8 @@
 //grain exercise
-export const square = () => {
+export const square = (N: number) => {
+  if (N < 1 || N > 64) {
+    throw new Error("Square must be between 1 and 64");
+  }
   console.log("square");
 };
 
