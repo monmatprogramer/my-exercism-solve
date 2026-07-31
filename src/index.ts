c68ef063dd4c437a9c3f2de94181493eb0e0e15c
Mon Mat <monsokmeanmon@gmail.com>
2026-07-31 08:37:24 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,11 @@ import { valid } from "./luhn";
 
 let d: string = "12 345 334 34";
 
-console.log(valid("59"));
+//console.log(valid("59"));
+
+let i: number = 0;
+
+do {
+  console.log(i);
+  i++;
+} while (i < 10);
