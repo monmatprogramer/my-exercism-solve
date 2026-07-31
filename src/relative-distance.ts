cac41332dfc19ef7c0c4c1768b692f9fb4c4febd
Mon Mat <monsokmeanmon@gmail.com>
2026-07-31 15:31:39 +0700

    update

---
+++ src/relative-distance.ts
@@ -0,0 +1,8 @@
+// relative distance
+export function degreesOfSeparation(
+  familyTree: unknown,
+  personA: unknown,
+  personB: unknown,
+): unknown {
+  throw new Error("Remove this line and implement the function");
+}
