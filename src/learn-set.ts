a20c9ce88b73bb0b0da1eed3175cfb78d2889d56
monmatprogramer <monsokmeanmon@gmail.com>
2026-08-01 19:05:13 +0700

    Update learn Set

--- src/learn-set.ts
+++ src/learn-set.ts
@@ -9,6 +9,8 @@ function lession1Fun() {
   // Method of set as following:
   // --- 1. add
   processedTransactions.add("transaction1");
+  // --- 2. check the value exist or not in the set list
+  console.log(processedTransactions.has("transaction1"));
   console.log("->", processedTransactions);
 }
 
