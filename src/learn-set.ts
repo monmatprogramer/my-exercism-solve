c9297aafe7c30486c679cc8d98917d33990aae00
monmatprogramer <monsokmeanmon@gmail.com>
2026-08-01 19:10:37 +0700

    Update learn Set

--- src/learn-set.ts
+++ src/learn-set.ts
@@ -15,6 +15,7 @@ function lession1Fun() {
   processedTransactions.delete("transaction1");
   // --- 4. Check size like check lenght of an array
   processedTransactions.size; //Set(0){}
+  // --- 5. Clear every value in the set list like delete whole an array element
   console.log("->", processedTransactions);
 }
 
