d4e486445cfbecc052d5fba76c5e7ef2fa460a93
Mon Mat <monsokmeanmon@gmail.com>
2026-08-03 16:24:15 +0700

    update code in exercise

--- src/relative-distance.ts
+++ src/relative-distance.ts
@@ -2,15 +2,13 @@
 export type FamilyTreeType = Record<string, string[]>;
 export function degreesOfSeparation(
   familyTree: FamilyTreeType,
-  personA: unknown,
-  personB: unknown,
+  personA: string,
+  personB: string,
 ): number {
   // creae an empty graph dictionary
   const graph: Map<string, string> = new Map<string, string>();
   // Read parent and children
   for (const [key, parent] of Object.entries(familyTree)) {
-    console.log(key);
-    console.log(parent);
   }
   return 0;
 }
