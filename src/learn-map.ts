9e1a4d7d1a56d6cec39f5e41d1f01fe557aa764b
monmatprogramer <monsokmeanmon@gmail.com>
2026-08-03 15:14:17 +0700

    update

--- src/learn-map.ts
+++ src/learn-map.ts
@@ -9,6 +9,7 @@ function toWayOne() {
   console.log(userAges);
   //{ Alice: 22, Bob: 34 }
 }
+
 // # 2. use Index (tranditional way)
 // - 2.1 create an  interface
 interface StringNumberDic {
@@ -22,4 +23,13 @@ function toWayTwo() {
   console.log(scores);
   //{ player1: 100, player2: 250 }
 }
-toWayTwo();
+
+// # 3. use Map
+function toWayThree() {
+  const featureFlags = new Map<string, boolean>();
+  featureFlags.set("darkMode", true);
+  featureFlags.set("betaFeatures", false);
+  const isDardMode = featureFlags.get("darkMode");
+  console.log(isDardMode); // true
+}
+toWayThree();
