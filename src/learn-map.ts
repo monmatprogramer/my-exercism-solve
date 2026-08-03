3dcb1120445b86d3648a385435f0c558f5d46e35
monmatprogramer <monsokmeanmon@gmail.com>
2026-08-03 15:11:29 +0700

    update

--- src/learn-map.ts
+++ src/learn-map.ts
@@ -7,8 +7,9 @@ function toWayOne() {
   };
   userAges["Alice"] = 22;
   console.log(userAges);
+  //{ Alice: 22, Bob: 34 }
 }
-// # 2. use Index
+// # 2. use Index (tranditional way)
 // - 2.1 create an  interface
 interface StringNumberDic {
   [key: string]: number;
@@ -19,5 +20,6 @@ function toWayTwo() {
     player2: 250,
   };
   console.log(scores);
+  //{ player1: 100, player2: 250 }
 }
 toWayTwo();
