fc8bcd169b5e52db1f67439b77cd18bbfceacf90
monmatprogramer <monsokmeanmon@gmail.com>
2026-08-05 19:13:18 +0700

    update home

--- src/learn-map.ts
+++ src/learn-map.ts
@@ -43,6 +43,10 @@ function toWayTwo() {
 function toWayThree() {
   const featureFlags = new Map<string, boolean>();
   const relatives = new Map<string, string[]>();
+  console.log(relatives.get("Mat"));
+  return;
+  //-- fill relative
+  relatives.set("Mat", ["Meng", "Mouy", "Heng"]);
   featureFlags.set("darkMode", true);
   featureFlags.set("betaFeatures", false);
   featureFlags.set("darkMode", false);
