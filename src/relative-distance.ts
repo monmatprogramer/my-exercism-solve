8ffec303a0a8fcab13ca878f145c1e5949ce5cc6
Mon Mat <monsokmeanmon@gmail.com>
2026-08-05 10:50:13 +0700

    learn map

--- src/relative-distance.ts
+++ src/relative-distance.ts
@@ -5,15 +5,8 @@ export function degreesOfSeparation(
   personA: string,
   personB: string,
 ): number {
-  // creae an empty graph dictionary
-  const graph: Map<string, string> = new Map<string, string>();
-  // Read parent and children
-  for (const [parent, children] of Object.entries(familyTree)) {
-    console.log("parent: ", parent);
-    for (const child of children) {
-      console.log(child);
-    }
-  }
+  // graph
+  const graph: Map<string, string> = addToGrap(familyTree);
   return 0;
 }
 
