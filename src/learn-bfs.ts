e3550dd00685070b21b27cf103801b7f9b9fdf90
Mon Mat <monsokmeanmon@gmail.com>
2026-08-06 13:56:45 +0700

    update code in learning relative

--- src/learn-bfs.ts
+++ src/learn-bfs.ts
@@ -1,5 +1,12 @@
 // Learn about breadth-first searching
 function bfs(graph: Record<string, string[]>, startNode: string) {
   // Create a queue
-  const queue: string[] = [];
+  const queue: string[] = [startNode];
+  console.log(queue);
 }
+
+const graph: Record<string, string[]> = {
+  A: ["B", "C"],
+  B: ["D"],
+};
+bfs(graph, "A");
