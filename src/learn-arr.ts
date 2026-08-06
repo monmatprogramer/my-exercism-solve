c635d0826230831d66de44253216bf5d100cd14f
monmatprogramer <monsokmeanmon@gmail.com>
2026-08-06 15:12:35 +0700

    update code in learning array

--- src/learn-arr.ts
+++ src/learn-arr.ts
@@ -91,6 +91,5 @@ function searchArrayInArray() {
   });
   console.log("\nMethod 3: ");
   console.log("---------");
-  console.log(arr2.filter((value) => arr1.map((v) => v === value)));
 }
 searchArrayInArray();
