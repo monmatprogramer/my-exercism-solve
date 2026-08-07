822f5e6045b16df3c4d895b2164ce95008f2e565
monmatprogramer <monsokmeanmon@gmail.com>
2026-08-07 10:17:52 +0700

    learn object

---
+++ src/learn-obj.ts
@@ -0,0 +1,32 @@
+// Learn about object start from scratch
+
+function lesson1() {
+  interface ServerInfo {
+    brand: string;
+    memory: number;
+  }
+
+  const myNAS: ServerInfo = {
+    brand: "QNAP",
+    memory: 4,
+  };
+}
+
+// properties of object is function
+function lesson2() {
+  interface BackendService {
+    name: string;
+    memory: number;
+    start: () => void;
+  }
+
+  const koyeb: BackendService = {
+    name: 'Koyeb',
+    memory: 8
+    start: () =>{
+      console.log(`${name} is start...`)
+    }
+  }
+  koyeb.start();
+}
+lesson2();
