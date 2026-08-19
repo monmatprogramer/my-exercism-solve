59d46f60d40e7b1697d3b4480bc771aee8fa5950
monmatprogramer <monsokmeanmon@gmail.com>
2026-08-19 11:36:50 +0700

    update code

--- src/acronym.ts
+++ src/acronym.ts
@@ -1,11 +1,24 @@
 // acronym
 export function parse(phrase: string): string {
   // expression removing punctuation
-  const expression = /^[a-zA-Z]$/;
-  console.log(expression.test(phrase));
+  const expression = /^[a-zA-Z\s-]+$/;
+  const hyphen = /^[a-zA-Z\-]+$/;
   //spliter
+  let spliter: string;
+  if (expression.test(phrase)) {
+    if (space.test(phrase)) {
+      spliter = " ";
+    } else {
+      spliter = "-";
+    }
+  }
   //Find the words
-  const findWords = phrase.split(" ");
+  const findWords = phrase.split(spliter!);
   console.log(findWords);
   return "";
 }
+const removeSpace = (rwPhrase: string): boolean => {
+  const space = /^[a-zA-Z\s]+$/;
+
+  return true;
+};
