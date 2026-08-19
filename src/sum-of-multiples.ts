fc5a94da5808e5627386e073e50672e734f708b4
monmatprogramer <monsokmeanmon@gmail.com>
2026-08-19 09:55:23 +0700

    update code

--- src/sum-of-multiples.ts
+++ src/sum-of-multiples.ts
@@ -1,9 +1,13 @@
 export function sum(baseValues: number[], multipler: number) {
   //creat a empty set to hold unqiue multiples
-  const finalResult = new Set<number>();
+  const total_sum = new Set<number>();
+  // set current multiples
+  let current_multiples: number[];
   // base value is empty
   if (baseValues.length === 0) {
-    return finalResult.add(0);
+    return total_sum.add(0);
+  } else {
+    current_multiples = baseValues;
   }
   return 0;
 }
