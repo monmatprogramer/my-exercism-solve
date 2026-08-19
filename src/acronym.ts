df993697b3b3113822e66b3b3e2d64d8ee8d330b
monmatprogramer <monsokmeanmon@gmail.com>
2026-08-19 11:25:10 +0700

    update code

--- src/acronym.ts
+++ src/acronym.ts
@@ -1,5 +1,9 @@
 // acronym
 export function parse(phrase: string): string {
+  // expression removing punctuation
+  const expression = /^[a-zA-Z]$/;
+  console.log(expression.test(phrase));
+  //spliter
   //Find the words
   const findWords = phrase.split(" ");
   console.log(findWords);
