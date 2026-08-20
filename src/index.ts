e42590dc93197ca447ccf1a45b1546e84d3d12e9
monmatprogramer <monsokmeanmon@gmail.com>
2026-08-20 15:07:27 +0700

    update code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Anagram } from "./anagram";
 try {
-  const subject = new Anagram("Orchestra");
-  const matches = subject.matches("cashregister", "Carthorse", "radishes");
+  const subject = new Anagram("ΑΒΓ");
+  const matches = subject.matches("ΒΓΑ", "ΒΓΔ", "γβα", "αβγ");
   console.log(matches);
 } catch (e: any) {
   console.log(e.message);
