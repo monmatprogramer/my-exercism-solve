1172ca636f55a7097b8040b756010af24fbf4945
monmatprogramer <monsokmeanmon@gmail.com>
2026-08-21 10:18:57 +0700

    update code

--- src/testing-code.ts
+++ src/testing-code.ts
@@ -1,1 +1,9 @@
 // This is for testing code
+
+function learnSortGreek() {
+  const tempResult: string[] = ["ΑΒΓ", "ΒΓΔ", "αβγ", "αβγ"];
+  const anagramStr = "ΑΒΓ";
+  console.log("γβα".toLowerCase());
+}
+
+learnSortGreek();
