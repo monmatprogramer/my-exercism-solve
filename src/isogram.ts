e426637ade30b2870e2071974127e3014f024d60
monmatprogramer <monsokmeanmon@gmail.com>
2026-08-21 18:36:55 +0700

    day 02 update

--- src/isogram.ts
+++ src/isogram.ts
@@ -2,13 +2,18 @@
 export function isIsogram(input: string): boolean {
   //conver to lowercase
   const lowerText = input.toLocaleLowerCase();
+  //cleaned text
+  let cleanedText: string = "";
   // clean text
   if (!cleanText(input)) {
     throw new Error("Not valid input");
   }
   if (checkSpace(input)) {
     //remove space
-    removeSpace(input);
+    cleanedText = removeSpace(input);
+  }
+  if (checkHyphen(input)) {
+    cleanedText = removeHyphen(input);
   }
   //remove hyphens
   return true;
@@ -17,6 +22,9 @@ export function isIsogram(input: string): boolean {
 const cleanText = (rwInput: string): boolean => {
   const clean_express = /^[a-zA-Z\s-]+$/g;
   return clean_express.test(rwInput);
+};
+const checkHyphen = (inpput: string): boolean => {
+  return /[\-]/g.test(input);
 };
 const checkSpace = (input: string): boolean => {
   return /[\s]/g.test(input);
