3e5f7d2f4f34418f8c41a1bf216d1a791e41ce0a
monmatprogramer <monsokmeanmon@gmail.com>
2026-08-21 19:19:03 +0700

    day 02 update

--- src/roman-numerals.ts
+++ src/roman-numerals.ts
@@ -1,2 +1,34 @@
 //find roman number
-export const toRoman = () => {};
+// Magic table
+const magicTable: Record<number, string> = {
+  1000: "M",
+
+  900: "CM",
+
+  500: "D",
+
+  400: "CD",
+
+  100: "C",
+
+  90: "XC",
+
+  50: "L",
+
+  40: "XL",
+
+  10: "X",
+
+  9: "IX",
+
+  5: "V",
+
+  4: "IV",
+
+  1: "I",
+};
+
+export const toRoman = (num: number): string => {
+  let result: string = "";
+  return result;
+};
