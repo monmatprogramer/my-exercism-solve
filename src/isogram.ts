5f32515bd299ce7e4d8ed78452d4fe299f7985cb
monmatprogramer <monsokmeanmon@gmail.com>
2026-08-21 16:45:54 +0700

    update code

--- src/isogram.ts
+++ src/isogram.ts
@@ -7,11 +7,17 @@ export function isIsogram(input: string): boolean {
     throw new Error("Not valid input");
   }
   //remove space
+
   //remove hyphens
   return true;
 }
 
 const cleanText = (rwInput: string): boolean => {
-  const clean_express = /^[a-zA-Z\s-]/g;
+  const clean_express = /^[a-zA-Z\s-]+$/g;
   return clean_express.test(rwInput);
+};
+const removeSpace = (input: string): string => {
+  const notSpace_exp = /[]/g;
+  let notSpace: string = "";
+  return notSpace;
 };
