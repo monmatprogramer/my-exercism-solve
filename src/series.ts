b421dffb25ff99d1b8bfcd083d27cd58c109488f
monmatprogramer <monsokmeanmon@gmail.com>
2026-08-22 11:35:53 +0700

    update code

---
+++ src/series.ts
@@ -0,0 +1,9 @@
+export class Series {
+  constructor(series: unknown) {
+    throw new Error("Remove this line and implement the function");
+  }
+
+  slices(sliceLength: unknown): unknown {
+    throw new Error("Remove this line and implement the function");
+  }
+}
