19411708547623459062301b2d49fbbfc28b3cf5
Mon Mat <monsokmeanmon@gmail.com>
2026-08-24 08:39:18 +0700

    update

--- src/series.ts
+++ src/series.ts
@@ -1,10 +1,13 @@
 export class Series {
-  private seriesString: number[];
+  private seriesString: string;
   constructor(series: string) {
     this.seriesString = series;
   }
 
   slices(sliceLength: number): number[][] {
+    //conver to num array string
+    //
+    const numStringArr : number[] [] =
     const result: number[][] = [];
     for (const [index, char] of this.seriesString) {
       result.push(this.seriesString.slice(index, sliceLength));
