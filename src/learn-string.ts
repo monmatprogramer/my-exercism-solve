a6d9cd999a8de1f114d75ad1ea69d5b4fc0999f9
monmatprogramer <monsokmeanmon@gmail.com>
2026-08-24 15:40:59 +0700

    update code

--- src/learn-string.ts
+++ src/learn-string.ts
@@ -5,14 +5,19 @@ const findString = () => {
   // method 1
   console.log("method 1");
   for (const char of st1) {
-    console.log(char === "X");
+    console.log(char === "X"); // true, false , fasle
   }
   console.log("-----End method  1--- \n\n");
   // method 2
   console.log("method 2");
   let xValue = st1.split("").filter((value) => value === "X");
-  console.log(xValue);
-  console.log("-----End method  1--- \n\n");
+  console.log(xValue); // ['X']
+  console.log("-----End method  2--- \n\n");
+
+  console.log("method 3");
+
+  console.log(); // ['X']
+  console.log("-----End method  3--- \n\n");
 };
 
 findString();
