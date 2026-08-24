3bc270e6c9a6ca813e79ec64f93a3f356ba28f33
monmatprogramer <monsokmeanmon@gmail.com>
2026-08-24 14:19:39 +0700

    update code

--- src/series.ts
+++ src/series.ts
@@ -14,15 +14,19 @@ export class Series {
     return (result = method2(convertToNum, copyConverToNum, sliceLength));
   }
 }
-const checkSliceLength = (): never => {
-  throw new Error("slice length cannot be greater than series length");
-};
+
 const method2 = (
   orinalArr: number[],
   copyArr: number[],
   sliceLength: number,
 ): number[][] => {
+  if (sliceLength > orinalArr.length) {
+    throw new Error("slice length cannot be greater than series length");
+  } else if (sliceLength === 0) {
+    throw new Error("slice length cannot be zero");
+  }
   const tempResult: number[][] = [];
+
   for (const [value] of orinalArr.entries()) {
     let sliceValueArr: number[] = copyArr.slice(0, sliceLength);
     if (sliceValueArr.length < sliceLength) {
