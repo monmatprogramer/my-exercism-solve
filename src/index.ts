4c89ba689fef57d300b7901e22fe255ac9999c0c
monmatprogramer <monsokmeanmon@gmail.com>
2026-08-24 19:01:09 +0700

    update code

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import { gamestate } from "./state-of-tic-tac-toe";
-const board = ["XXO", "XXX", "OOO"];
+//const board = ["XXO", "XXX", "OOO"];
+const board = ["OO", "O O", "XXX"];
 gamestate(board);
