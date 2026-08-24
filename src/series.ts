f837bfe2071ffc04239bfe997ea8b6222728644b
monmatprogramer <monsokmeanmon@gmail.com>
2026-08-24 10:34:30 +0700

    update code

--- src/series.ts
+++ src/series.ts
@@ -5,7 +5,6 @@ export class Series {
   }
 
   slices(sliceLength: number): number[][] {
-    console.log("sliceLength: ", sliceLength);
     //conver to num array string
     const convertToNum: number[] = this.seriesString.split("").map(Number);
     //TODO: Fix here
@@ -13,11 +12,12 @@ export class Series {
     const result: number[][] = [];
     let tempArr: number[] = [];
     for (let i: number = 0; i < convertToNum.length; i++) {
-      console.log("i: ", i);
-      console.log(copyConverToNum.slice(i, sliceLength));
-      result.push();
+      // console.log("i: ", i);
+      // console.log(copyConverToNum.slice(i, sliceLength));
+      tempArr = copyConverToNum.slice(i, sliceLength);
+      result.push(tempArr);
       sliceLength++;
-      console.log("\n");
+      // console.log("\n");
     }
     return result.filter((v) => v.length > 0);
   }
