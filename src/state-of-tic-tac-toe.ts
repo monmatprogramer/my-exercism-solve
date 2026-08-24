c6eec7e59db9d92f0f4447a5e3666d266befcb88
monmatprogramer <monsokmeanmon@gmail.com>
2026-08-24 15:29:33 +0700

    update code

--- src/state-of-tic-tac-toe.ts
+++ src/state-of-tic-tac-toe.ts
@@ -2,7 +2,7 @@
 export const gamestate = (board: string[]): string => {
   //count X the given board
   let totalXvalue: number = board.filter((xValue) =>
-    //TODO: Find X value here
+    console.log(xValue),
   ).length;
   console.log(totalXvalue); //3
   return "";
