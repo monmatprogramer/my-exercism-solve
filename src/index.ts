e44d04ebec3aaa10f70a7bcdab2a0254bb1c2096
monmatprogramer <monsokmeanmon@gmail.com>
2026-08-25 14:27:41 +0700

    update tic tae toe

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { gamestate } from "./state-of-tic-tac-toe";
 //const board = ["XXO", "XXX", "OOO"];
-const board = ["OOX", "X  ", "X  "];
+const board = ["XOO", " X ", " X "];
 console.log(gamestate(board));
