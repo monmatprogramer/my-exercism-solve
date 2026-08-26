3f7f172e2af3f61faea86950dcceb360c2259f70
monmatprogramer <monsokmeanmon@gmail.com>
2026-08-26 12:54:38 +0700

    update code

--- src/Exercism/phone-number/phone-number.ts
+++ src/Exercism/phone-number/phone-number.ts
@@ -1,3 +1,13 @@
 export function clean(phoneNumber: string) {
   console.log("phone string: ", phoneNumber);
+  checkForLetters(phoneNumber);
 }
+
+//Check for letters
+const checkForLetters = (letters: string): boolean => {
+  // define Regex
+  const letterRegex = /^[A-Za-z]+$/;
+  console.log("Result check: ", letterRegex.test(letters));
+  // return
+  return false;
+};
