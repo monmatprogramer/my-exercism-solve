c35a8405a3c30efaceb1231cfe28ceffb9c64ca4
monmatprogramer <monsokmeanmon@gmail.com>
2026-08-26 15:28:08 +0700

    update code

--- src/Exercism/phone-number/index.ts
+++ src/Exercism/phone-number/index.ts
@@ -1,5 +1,7 @@
 import { clean } from "./phone-number.ts";
 
 let ph: string = "12234567890";
-let test = clean("12234567890");
-console.log(test);
+// let test = clean("12234567890");
+// console.log(test);
+console.log(ph.split("").splice(0, 1));
+console.log(ph);
