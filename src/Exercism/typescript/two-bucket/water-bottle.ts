240b36c4dd4378cfd3733b71e97a491a55cf9efe
monmatprogramer <monsokmeanmon@gmail.com>
2026-08-27 15:04:00 +0700

    update code in new exercise

--- src/Exercism/typescript/two-bucket/water-bottle.ts
+++ src/Exercism/typescript/two-bucket/water-bottle.ts
@@ -16,5 +16,14 @@ class WaterBottle {
   addWater(amount: number) {
     if (amount > this.capacity) throw new Error("Cannot overflow");
     if (amount === 0) throw new Error("It is empty amount of water to add");
+    this.currentWater = amount;
+    console.log("currentWater: ", this.currentWater);
   }
+}
+
+const waterBottle = new WaterBottle(5);
+try {
+  waterBottle.addWater(6);
+} catch (e: any) {
+  console.log("💥 ", e.message);
 }
