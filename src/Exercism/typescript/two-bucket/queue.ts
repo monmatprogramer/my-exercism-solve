8c70a636d81190a5d8365209953163d36c7d28ad
monmatprogramer <monsokmeanmon@gmail.com>
2026-08-28 16:44:20 +0700

    update code

--- src/Exercism/typescript/two-bucket/queue.ts
+++ src/Exercism/typescript/two-bucket/queue.ts
@@ -4,8 +4,11 @@
 const runQueue = () => {
   // queue
   const myQueue: number[] = [10, 20, 30];
+  let currentNumber: number;
   // Remove first elment
   while (myQueue.length > 0) {
-    // start remove
+    // start Remove
+    currentNumber = myQueue.shift();
+    console.log("Current number: ", currentNumber);
   }
 };
