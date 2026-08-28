44823df6fd719b59151e01e0efc3b49a86b4e8bc
monmatprogramer <monsokmeanmon@gmail.com>
2026-08-28 14:21:47 +0700

    update code

--- src/Exercism/typescript/two-bucket/water-bottle.ts
+++ src/Exercism/typescript/two-bucket/water-bottle.ts
@@ -21,51 +21,6 @@ class WaterBottle {
     console.log("Current water: ", this.currentWater);
     console.log("Capacity: ", this.capacity + "L");
     console.log("--------------------------------\n");
-    if (amount === 0) {
-      console.log(
-        "The amount of water is empty. Please add not empty amount of water.",
-      );
-      return;
-    }
-    // Not replace the exsiting water
-    if (this.currentWater !== 0) {
-      console.log(`Bottle has existing water ${this.currentWater}L`);
-    }
-    // If it is full
-    if (this.currentWater === this.capacity) {
-      console.log(`The water in the bottle is full (${this.currentWater}L)`);
-      return;
-    }
-    // This give message about remaining water
-    if (this.currentWater < this.capacity) {
-      if (this.currentWater !== 0) {
-        overflow = amount - allow;
-      }
-      if (this.currentWater === 0) {
-        console.log("The bottle is empty water");
-      } else if (overflow < 0) {
-        console.log(`Reequire ${Math.abs(overflow)}L to fill in the bottle`);
-      } else {
-        console.log(`The overflow ${overflow}L from the bottle`);
-      }
-    }
-
-    if (allow > 0) {
-      console.log(
-        `You is allowed to add only ${this.capacity - this.currentWater} into the bottle`,
-      );
-    }
-    if (allow < this.capacity) {
-      this.currentWater += allow;
-      console.log(`Start adding ${allow}L....`);
-      if (this.currentWater < this.capacity) {
-        console.log(
-          `You have ${this.capacity - this.currentWater} to fill it.`,
-        );
-      }
-      console.log("current after adding the water: ", this.currentWater);
-    }
-    console.log("----------End adding water---------");
   }
 }
 
