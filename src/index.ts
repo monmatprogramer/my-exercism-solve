36ec3f49c861809df7216f5f768e89800ab0097f
monmatprogramer <monsokmeanmon@gmail.com>
2026-09-02 19:02:53 +0700

    update code

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import { gamestate } from "./state-of-tic-tac-toe";
-//const board = ["XXO", "XXX", "OOO"];
-// const board = ["OXX", "OX ", "O  "];
-// const board = ["XOO", "X  ", "X  "]; pass
-// const board = ["OXO", " X ", " X "]; pass
-const board = ["OOX", "  X", "  X"];
-// const board = ["OXX", "OX ", "O  "];
-console.log(gamestate(board));
+const arrayOb = [
+  {
+    id: "0001",
+    name: "Mon Mat",
+    age: 12,
+  },
+  {
+    id: "0002",
+    name: "Channy",
+    age: 22,
+  },
+];
